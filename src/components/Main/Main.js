67df48475ee37d0614393cee318912dbd3f3d841
Lauren <laurenmontenegro10@gmail.com>
2018-08-09 15:09:14 -0600

    Added Nav component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import './index.css';
+import Nav from "../Nav/Nav";
 
 class Main extends Component {
     render() {
         return (
             <div className="main">
-
+                <Nav/>
             </div>
         );
     }
