d3c19ce2957a4ceb8c92182dd3af3f48c441e51c
Lauren <laurenmontenegro10@gmail.com>
2018-08-10 15:09:55 -0600

    Refactored styles

--- src/components/MainContainer/MainContainer.js
+++ src/components/MainContainer/MainContainer.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import CompanyCard from "../CompanyCard/CompanyCard";
 import './index.css';
+import Toolbar from "../Toolbar/Main/Toolbar";
 
 class MainContainer extends Component {
     render() {
         return (
             <div className="container">
+                <Toolbar/>
                 <CompanyCard/>
                 <CompanyCard/>
                 <CompanyCard/>
