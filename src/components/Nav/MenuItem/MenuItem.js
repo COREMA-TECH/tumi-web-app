bb5e19c4a70c9ec06f784829b2c616fca7a133cc
Olonyl Landeros <jose87.landeros@gmail.com>
2018-08-14 10:15:58 -0600

    Notification Popper Integrated

--- src/components/Nav/MenuItem/MenuItem.js
+++ src/components/Nav/MenuItem/MenuItem.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import './index.css';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-const MenuItem = props => <Link to={props.item} className="menu__item">{props.item}</Link>;
+const MenuItem = (props) => (
+	<Link to={`/${props.item}`} className="menu__item">
+		{props.item}
+	</Link>
+);
 
 export default MenuItem;
