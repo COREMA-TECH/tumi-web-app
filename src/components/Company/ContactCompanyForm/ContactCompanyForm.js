655a0fc16471821251073dedc4282cc20298c7ce
Olonyl Landeros <jose87.landeros@gmail.com>
2018-08-17 08:26:58 -0600

    Insert Contacts Mutation Implemented

--- src/components/Company/ContactCompanyForm/ContactCompanyForm.js
+++ src/components/Company/ContactCompanyForm/ContactCompanyForm.js
@@ -6,7 +6,13 @@ import InputLabel from '@material-ui/core/InputLabel';
 import FormControl from '@material-ui/core/FormControl';
 import Button from '@material-ui/core/Button';
 import ContactsTable from '../ContactsTable/ContactsTable';
-
+import FormErrors from './FormErrors';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
+import LinearProgress from '@material-ui/core/LinearProgress';
 const styles = (theme) => ({
 	container: {
 		display: 'flex',
@@ -31,41 +37,135 @@ const styles = (theme) => ({
 		display: 'none'
 	}
 });
+const currencies = [
+	{
+		label: 'USD',
+		value: 1
+	},
+	{
+		label: 'EUR',
+		value: 2
+	},
+	{
+		label: 'BTC',
+		value: 3
+	},
+	{
+		label: 'JPY',
+		value: 4
+	}
+];
 
 class ContactCompanyForm extends React.Component {
-	state = {
-		name: '',
-		data: [],
-		username: '',
-		email: '',
-		number: '',
-		kind: ''
-	};
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: '',
+			data: [],
+			email: '',
+			number: '',
+			kind: '',
+			formErrors: { username: '', email: '', number: '', kind: '' },
+			usernameValid: false,
+			emailValid: false,
+			numberValid: false,
+			kindValid: false,
+			formValid: false
+		};
+	}
 
-	handleChange = (event) => {
-		this.setState({ name: event.target.value });
+	handleChange = (name) => (event) => {
+		this.setState({
+			[name]: event.target.value
+		});
 	};
+	handleUserInput(e) {
+		const name = e.target.name;
+		const value = e.target.value;
+		this.setState({ [name]: value }, () => {
+			this.validateField(name, value);
+		});
+	}
+	validateField(fieldName, value) {
+		let fieldValidationErrors = this.state.formErrors;
+		let emailValid = this.state.emailValid;
+		let usernameValid = this.state.usernameValid;
+		let numberValid = this.state.numberValid;
+		let kindValid = this.state.kindValid;
+
+		switch (fieldName) {
+			case 'email':
+				emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+				fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+				break;
+			case 'username':
+				usernameValid = value.length >= 6;
+				fieldValidationErrors.name = usernameValid ? '' : ' is too short';
+				break;
+			case 'number':
+				numberValid = value.length >= 6;
+				fieldValidationErrors.name = numberValid ? '' : ' is too short';
+				break;
+			case 'kind':
+				kindValid = value != null;
+				fieldValidationErrors.name = kindValid ? '' : ' must select type';
+				break;
+			default:
+				break;
+		}
+		this.setState(
+			{
+				formErrors: fieldValidationErrors,
+				emailValid: emailValid,
+				usernameValid: usernameValid,
+				numberValid: numberValid,
+				kindValid: kindValid
+			},
+			this.validateForm
+		);
+	}
+
+	validateForm() {
+		this.setState({
+			formValid:
+				this.state.emailValid && this.state.usernameValid && this.state.numberValid && this.state.kindValid
+		});
+	}
 
 	render() {
 		const { classes } = this.props;
 
+		const INSERT_CONTACTS = gql`
+			mutation inscontactscompany($input: iParamC!) {
+				inscontactscompany(input: $input) {
+					Id
+					Full_Name
+				}
+			}
+		`;
+
 		return (
 			<div className={classes.container}>
 				<div className={classes.divStyle}>
+					<div className="panel panel-default">
+						<FormErrors formErrors={this.state.formErrors} />
+					</div>
 					<FormControl className={classes.formControl}>
 						<InputLabel htmlFor="name-simple">Name</InputLabel>
 						<Input
 							id="name-simple"
+							name="username"
 							value={this.state.username}
-							onChange={(text) => this.setState({ username: text.target.value })}
+							onChange={(event) => this.handleUserInput(event)}
 						/>
 					</FormControl>
 					<FormControl className={classes.formControl}>
 						<InputLabel htmlFor="name-simple">Email</InputLabel>
 						<Input
 							id="name-simple"
+							name="email"
 							value={this.state.email}
-							onChange={(text) => this.setState({ email: text.target.value })}
+							onChange={(event) => this.handleUserInput(event)}
 						/>
 					</FormControl>
 
@@ -73,45 +173,87 @@ class ContactCompanyForm extends React.Component {
 						<InputLabel htmlFor="name-simple">Phone Number</InputLabel>
 						<Input
 							id="name-simple"
+							name="number"
 							value={this.state.number}
-							onChange={(text) => this.setState({ number: text.target.value })}
+							onChange={(event) => this.handleUserInput(event)}
 						/>
 					</FormControl>
 					<FormControl className={classes.formControl}>
-						<InputLabel htmlFor="name-simple">Kind Of Contact</InputLabel>
-						<Input
-							id="name-simple"
-							value={this.state.kind}
-							onChange={(text) => this.setState({ kind: text.target.value })}
-						/>
+						<form noValidate autoComplete="off">
+							<TextField
+								id="select-currency"
+								select
+								name="kind"
+								value={this.state.kind}
+								onChange={(event) => this.handleUserInput(event)}
+								helperText="Please select the type"
+								margin="normal"
+							>
+								{currencies.map((option) => (
+									<MenuItem key={option.value} value={option.value} name={'option.name'}>
+										{option.label}
+									</MenuItem>
+								))}
+							</TextField>
+						</form>
 					</FormControl>
-					<Button
-						variant="contained"
-						color="primary"
-						className={classes.button}
-						onClick={() => {
-							var item = {
-								username: this.state.username,
-								email: this.state.email,
-								number: this.state.number,
-								kind: this.state.kind
-							};
+					<Mutation mutation={INSERT_CONTACTS}>
+						{(inscontactscompany, { loading, error }) => (
+							<Button
+								disabled={!this.state.formValid}
+								variant="contained"
+								color="primary"
+								className={classes.button}
+								onClick={() => {
+									var item = {
+										username: this.state.username,
+										email: this.state.email,
+										number: this.state.number,
+										kind: this.state.kind
+									};
 
-							this.setState((prevState) => ({
-								// data: prevState.data.concat(item),
-								data: [ item, ...prevState.data ]
-							}));
+									inscontactscompany({
+										variables: {
+											input: {
+												Id: 0,
+												Id_Company: 1,
+												Full_Name: `'${this.state.username}'`,
+												Electronic_Address: `'${this.state.email}'`,
+												Phone_Number: `'${this.state.number}'`,
+												Contact_Type: this.state.kind,
+												IsActive: 1,
+												User_Created: 1,
+												User_Updated: 1,
+												Date_Created: "'2018-08-14 16:10:25+00'",
+												Date_Updated: "'2018-08-14 16:10:25+00'"
+											}
+										}
+									});
+									this.setState((prevState) => ({
+										data: prevState.data.concat(item),
+										data: [ item, ...prevState.data ]
+									}));
 
-							this.setState({
-								username: '',
-								email: '',
-								number: '',
-								kind: ''
-							});
-						}}
-					>
-						Add Contact
-					</Button>
+									this.setState({
+										username: '',
+										email: '',
+										number: '',
+										kind: '',
+										formErrors: { name: '', email: '', number: '', kind: '' },
+										nameValid: false,
+										emailValid: false,
+										numberValid: false,
+										kindValid: false,
+										formValid: false
+									});
+								}}
+							>
+								Add Contact
+								{loading && alert('loading')}
+								{error && <p>Error :( Please try again</p>}
+							</Button>
+						)}
+					</Mutation>
 				</div>
 				<div className={classes.divStyle}>
 					<ContactsTable data={this.state.data} />
