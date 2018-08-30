e3814eb04e7a1f37155444c5fabf5ef1e27a0b31
Lauren <laurenmontenegro10@gmail.com>
2018-08-30 09:28:22 -0600

    Added login route and fixed error

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import './index.css';
-import Nav from '../Nav/Main';
-import MainContainer from '../MainContainer/Main/MainContainer';
 import Route from "react-router-dom/es/Route";
 import Login from "../Login/Login";
 
@@ -9,9 +7,9 @@ class Main extends Component {
     render() {
         return (
             <div className="main">
-                <Route path="/login" component={Login} />
-                {/*<Nav/>*/}
-                {/*<MainContainer/>*/}
+                <Route path="/login" component={Login}/>
+                <Nav/>
+                <MainContainer/>
             </div>
         );
     }
