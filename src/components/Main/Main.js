96bc0c4c462661056660caa40196047abcc0f9fd
Olonyl Landeros <jose87.landeros@gmail.com>
2018-09-03 15:34:58 -0600

    Login deleted

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,18 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './index.css';
-import Route from "react-router-dom/es/Route";
-import Login from "../Login/Login";
-import Private from "../Private/Private";
-import PrivateRoute from "../PrivateRoute/PrivateRoute";
+import Route from 'react-router-dom/es/Route';
+
+import Private from '../Private/Private';
+import PrivateRoute from '../PrivateRoute/PrivateRoute';
 
 class Main extends Component {
-    render() {
-        return (
-            <div>
-                <Route path="/login" component={Login}/>
-                <PrivateRoute path="/home" component={Private}/>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				<PrivateRoute path="/home" component={Private} />
+			</div>
+		);
+	}
 }
 
 export default Main;
