76b686649ddbed065a4cf7a149be7ffdb9eee4d5
Olonyl Landeros <jose87.landeros@gmail.com>
2018-09-11 09:58:51 -0600

    Font Style and Color behavior in signature form

--- src/components/Contract/Signature/Signature.js
+++ src/components/Contract/Signature/Signature.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './index.css';
 import SignaturePad from 'react-signature-canvas';
 
-import withMobileDialog from '@material-ui/core/withMobileDialog';
 import { withApollo } from 'react-apollo';
 import PropTypes from 'prop-types';
 import SaveIcon from '@material-ui/icons/Save';
@@ -10,7 +9,7 @@ import ClearIcon from '@material-ui/icons/Clear';
 import ClearAllIcon from '@material-ui/icons/ClearAll';
 import Button from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip';
-import CheckIcon from '@material-ui/icons/Check';
+import TitleIcon from '@material-ui/icons/Title';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
@@ -19,6 +18,16 @@ import { Snackbar } from '@material-ui/core';
 import { MySnackbarContentWrapper } from '../../Generic/SnackBar';
 import LinearProgress from '@material-ui/core/es/LinearProgress/LinearProgress';
 import gql from 'graphql-tag';
+
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import withMobileDialog from '@material-ui/core/withMobileDialog';
+
+import DomToImage from 'dom-to-image';
+import { domainToUnicode } from 'url';
 
 const styles = (theme) => ({
 	container: {
@@ -32,6 +41,7 @@ const styles = (theme) => ({
 
 	buttonSuccess: {
 		backgroundColor: green[500],
+		color: 'white',
 		'&:hover': {
 			backgroundColor: green[700]
 		}
@@ -69,7 +79,10 @@ class Signature extends React.Component {
 			messageSnackbar: 'Dummy text!',
 			agreement: '',
 			signature: '',
-			loadingData: false
+			loadingData: false,
+			openModal: false,
+			selectedColor: 'blackSelector',
+			selectedLetter: 'letter1Selector'
 		};
 	}
 
@@ -94,7 +107,7 @@ class Signature extends React.Component {
 		// and only part of the canvas is cleared then.
 		var ratio = Math.max(window.devicePixelRatio || 1, 1);
 		var canvas = this.sigPad.getCanvas();
-		var signaturePad = this.sigPad;
+
 		// This part causes the canvas to be cleared
 		canvas.width = canvas.offsetWidth * ratio;
 		canvas.height = canvas.offsetHeight * ratio;
@@ -106,6 +119,26 @@ class Signature extends React.Component {
 		// that the state of this library is consistent with visual state of the canvas, you
 		// have to clear it manually.
 		//	signaturePad.clear();
+	};
+	insertTextIntoCanvas = () => {
+		var node = document.getElementById('signatureContainer');
+		function getImage(dataUrl, parent) {
+			var ratio = Math.max(window.devicePixelRatio || 1, 1);
+			var canvas = parent.sigPad.getCanvas();
+
+			canvas.width = canvas.offsetWidth * ratio;
+			canvas.height = canvas.offsetHeight * ratio;
+			canvas.getContext('2d').scale(ratio, ratio);
+			parent.sigPad.fromDataURL(dataUrl);
+			parent.setState({ openModal: false, signature: dataUrl });
+		}
+		DomToImage.toPng(node)
+			.then((dataURL) => {
+				getImage(dataURL, this);
+			})
+			.catch(function(error) {
+				console.error('oops, something went wrong!', error);
+			});
 	};
 	loadAgreement = () => {
 		this.setState({ loadingData: true });
@@ -211,14 +244,100 @@ class Signature extends React.Component {
 		window.addEventListener('resize', this.resizeCanvas.bind(this));
 		this.resizeCanvas();
 	}
+	handleClickOpenModal = () => {
+		this.setState({ openModal: true });
+	};
+
+	handleCloseModal = () => {
+		this.setState({ openModal: false });
+	};
+
+	handleColorSelectorClick = (event) => {
+		this.setState({ selectedColor: event.currentTarget.id });
+	};
+	handleLetterSelectorClick = (event) => {
+		this.setState({ selectedLetter: event.currentTarget.id });
+	};
+	getClassColorSelector = (id) => {
+		switch (id) {
+			case 'blackSelector':
+				if (this.state.selectedColor == id) return 'signature-handwriting-color leftborder selected';
+				else return 'signature-handwriting-color leftborder';
+				break;
+			case 'blueSelector':
+				if (this.state.selectedColor == id) return 'signature-handwriting-color noleftborder selected';
+				else return 'signature-handwriting-color noleftborder';
+				break;
+			case 'greenSelector':
+				if (this.state.selectedColor == id) return 'signature-handwriting-color noleftborder selected';
+				else return 'signature-handwriting-color noleftborder';
+				break;
+			default:
+				return 'signature-handwriting-color leftborder';
+				break;
+		}
+	};
+	getClassLetterSelector = (id) => {
+		switch (id) {
+			case 'letter1Selector':
+				if (this.state.selectedLetter == id)
+					return 'default-font signature-handwriting-letter leftborder selected';
+				else return 'default-font signature-handwriting-letter leftborder';
+				break;
+			case 'letter2Selector':
+				if (this.state.selectedLetter == id)
+					return 'second-font signature-handwriting-letter noleftborder selected';
+				else return 'second-font signature-handwriting-letter noleftborder';
+				break;
+			case 'letter3Selector':
+				if (this.state.selectedLetter == id)
+					return 'third-font signature-handwriting-letter noleftborder selected';
+				else return 'third-font signature-handwriting-letter noleftborder';
+				break;
+			default:
+				return 'default-font signature-handwriting-letter leftborder';
+				break;
+		}
+	};
+	getClassTextInput = () => {
+		var font = 'signature-handwriting-input blackText';
+		switch (this.state.selectedColor) {
+			case 'blackSelector':
+				font = 'signature-handwriting-input blackText';
+				break;
+			case 'blueSelector':
+				font = 'signature-handwriting-input blueText';
+				break;
+			case 'greenSelector':
+				font = 'signature-handwriting-input greenText';
+				break;
+			default:
+				font = 'signature-handwriting-input blackText';
+				break;
+		}
+		switch (this.state.selectedLetter) {
+			case 'letter1Selector':
+				font += ' default-font';
+				break;
+			case 'letter2Selector':
+				font += ' second-font';
+				break;
+			case 'letter3Selector':
+				font += ' third-font';
+				break;
+			default:
+				font += ' default-font';
+				break;
+		}
+		return font;
+	};
 	render() {
 		const { classes } = this.props;
-		const { fullScreen } = this.props;
 		const buttonClassname = classNames({
 			[classes.buttonSuccess]: success
 		});
 		const { loading, success } = this.state;
-
+		const { fullScreen } = this.props;
 		return (
 			<div className="signature-container">
 				{this.state.loadingData && <LinearProgress />}
@@ -241,7 +360,9 @@ class Signature extends React.Component {
 				<div className="signature-content">
 					<textarea
 						type="text"
+						spellCheck="false"
 						value={this.state.agreement}
+						readOnly
 						className="signature-information"
 						placeholder={this.props.placeholder}
 					/>
@@ -263,21 +384,40 @@ class Signature extends React.Component {
 				</div>
 
 				<div className="signature-footer">
-					<div className="signature-button">
-						<Tooltip title={'Clear'}>
-							<div>
-								<Button
-									//disabled={this.state.loading || !this.state.enableCancelButton}
-									variant="fab"
-									color="secondary"
-									className={buttonClassname}
-									onClick={this.clearSignature}
-								>
-									<ClearAllIcon />
-								</Button>
-							</div>
-						</Tooltip>
+					<div className="signature-button-root">
+						<div className="signature-button">
+							<Tooltip title={'Clear'}>
+								<div>
+									<Button
+										disabled={this.state.loading}
+										variant="fab"
+										color="secondary"
+										className={buttonClassname}
+										onClick={this.clearSignature}
+									>
+										<ClearAllIcon />
+									</Button>
+								</div>
+							</Tooltip>
+						</div>
 					</div>
+					<div className="signature-button-root">
+						<div className="signature-button">
+							<Tooltip title="Write Signature">
+								<div>
+									<Button
+										disabled={this.state.loading}
+										variant="fab"
+										className={[ buttonClassname, classes.buttonSuccess ].join(' ')}
+										onClick={this.handleClickOpenModal}
+									>
+										<TitleIcon />
+									</Button>
+								</div>
+							</Tooltip>
+						</div>{' '}
+					</div>
+
 					<div className="signature-button-root">
 						<div className="signature-button-wrapper">
 							<div className="signature-button">
@@ -300,6 +440,79 @@ class Signature extends React.Component {
 						</div>
 					</div>
 				</div>
+				<Dialog
+					fullScreen={fullScreen}
+					open={this.state.openModal}
+					onClose={this.handleCloseModal}
+					aria-labelledby="responsive-dialog-title"
+				>
+					<DialogTitle id="responsive-dialog-title">{'Write Signature'}</DialogTitle>
+					<DialogContent className="signature-handwriting-dialog-content">
+						<div className="signature-handwriting-color-container">
+							<div
+								id="blackSelector"
+								className={this.getClassColorSelector('blackSelector')}
+								onClick={this.handleColorSelectorClick}
+							>
+								<div className="circleBase black" />
+							</div>
+							<div
+								id="greenSelector"
+								className={this.getClassColorSelector('greenSelector')}
+								onClick={this.handleColorSelectorClick}
+							>
+								<div className="circleBase green" />
+							</div>
+							<div
+								id="blueSelector"
+								className={this.getClassColorSelector('blueSelector')}
+								onClick={this.handleColorSelectorClick}
+							>
+								<div className="circleBase blue" />
+							</div>
+						</div>
+						<div className="signature-handwriting-container">
+							<textarea
+								id="signatureContainer"
+								type="text"
+								spellCheck="false"
+								className={this.getClassTextInput()}
+								placeholder={'Write Signature'}
+							/>
+						</div>
+						<div className="signature-handwriting-letter-container">
+							<div
+								id="letter1Selector"
+								className={this.getClassLetterSelector('letter1Selector')}
+								onClick={this.handleLetterSelectorClick}
+							>
+								Text
+							</div>
+							<div
+								id="letter2Selector"
+								className={this.getClassLetterSelector('letter2Selector')}
+								onClick={this.handleLetterSelectorClick}
+							>
+								Text
+							</div>
+							<div
+								id="letter3Selector"
+								className={this.getClassLetterSelector('letter3Selector')}
+								onClick={this.handleLetterSelectorClick}
+							>
+								Text
+							</div>
+						</div>
+					</DialogContent>
+					<DialogActions>
+						<Button onClick={this.insertTextIntoCanvas} color="primary">
+							Agree
+						</Button>
+						<Button onClick={this.handleCloseModal} color="primary" autoFocus>
+							Degree
+						</Button>
+					</DialogActions>
+				</Dialog>
 			</div>
 		);
 	}
