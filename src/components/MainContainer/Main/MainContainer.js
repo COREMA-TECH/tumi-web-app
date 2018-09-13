60cff5dcd31f1a421a70239d5775896afae8c009
LaurenceM10 <laurenmontenegro10@gmail.com>
2018-09-12 21:57:23 -0600

    Added method to open the nav in mobile version

--- src/components/MainContainer/Main/MainContainer.js
+++ src/components/MainContainer/Main/MainContainer.js
@@ -4,11 +4,16 @@ import Toolbar from '../Toolbar/Main/Toolbar';
 import Container from '../Container/';
 
 class MainContainer extends Component {
+
+    handleClickMenu = (event) => {
+        //Open the menu
+    };
+
     render() {
         return (
             <div className="main-container">
                 <div className="main-container--header">
-                    <span className="icon-menu"></span>
+                    <span className="icon-menu" onClick={this.handleClickMenu}></span>
                     <span className="main-container__title">Company Information</span>
                     <Toolbar/>
                 </div>
