1cf4f7e316dced677c951816cf8cfbb1088d6f31
Olonyl Landeros <jose87.landeros@gmail.com>
2018-09-13 15:45:44 -0600

    Edit Button into contract table

--- src/components/Contract/Main/MainContract/MainContract.js
+++ src/components/Contract/Main/MainContract/MainContract.js
@@ -1,74 +1,79 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './index.css';
-import {gql} from 'apollo-boost';
-import withApollo from "react-apollo/withApollo";
-import LinearProgress from "@material-ui/core/LinearProgress/LinearProgress";
-import TablesContracts from "./TablesContracts";
-import CircularProgress from "../../../material-ui/CircularProgress";
-import {Query} from "react-apollo";
-import NothingToDisplay from "../../../ui-components/NothingToDisplay/NothingToDisplay";
+import { gql } from 'apollo-boost';
+import withApollo from 'react-apollo/withApollo';
+import LinearProgress from '@material-ui/core/LinearProgress/LinearProgress';
+import TablesContracts from './TablesContracts';
+import CircularProgress from '../../../material-ui/CircularProgress';
+import { Query } from 'react-apollo';
+import NothingToDisplay from '../../../ui-components/NothingToDisplay/NothingToDisplay';
 
 class MainContract extends Component {
-    constructor(props) {
-        super(props);
+	constructor(props) {
+		super(props);
 
-        this.state = {
-            loadingContracts: false,
-            data: [],
-            loadingRemoving: false
-        }
-    }
+		this.state = {
+			loadingContracts: false,
+			data: [],
+			loadingRemoving: false
+		};
+	}
 
-    /**
+	/**
      * This method redirect to create contract component
      */
-    redirectToCreateContract = () => {
-        this.props.history.push('/home/contract/add');
-    };
+	redirectToCreateContract = () => {
+		this.props.history.push('/home/contract/add');
+	};
 
-    getContractsQuery = gql`
-             {
-                getcontracts(Id: null, IsActive: 1) {
-                     Id
-                     Contract_Name
-                     Contrat_Owner
-                     Contract_Status
-                     Contract_Expiration_Date
-                }
-             }
-    `;
+	getContractsQuery = gql`
+		{
+			getcontracts(Id: null, IsActive: 1) {
+				Id
+				Contract_Name
+				Contrat_Owner
+				Contract_Status
+				Contract_Expiration_Date
+			}
+		}
+	`;
 
-    getContracts = () => {
-        // Show linear progress
-        this.setState({
-            loadingContracts: true,
-        }, () => {
-        });
+	getContracts = () => {
+		// Show linear progress
+		this.setState(
+			{
+				loadingContracts: true
+			},
+			() => {}
+		);
 
-        this.props.client
-            .query({
-                query: this.getContractsQuery,
-                fetchPolicy: 'no-cache',
-            })
-            .then(({data}) => {
-                this.setState(prevState => ({
-                    data: [...prevState.data, data.getcontracts]
-                }), () => {
-                    // Hide linear progress
-                    this.setState({
-                        loadingContracts: false,
-                    });
-                });
-            })
-            .catch(error => {
-                console.log("Error fetching data")
-            })
-    };
+		this.props.client
+			.query({
+				query: this.getContractsQuery,
+				fetchPolicy: 'no-cache'
+			})
+			.then(({ data }) => {
+				this.setState(
+					(prevState) => ({
+						data: [ ...prevState.data, data.getcontracts ]
+					}),
+					() => {
+						// Hide linear progress
+						this.setState({
+							loadingContracts: false
+						});
+					}
+				);
+			})
+			.catch((error) => {
+				console.log('Error fetching data');
+			});
+	};
 
-    /**
+	/**
      * To delete contracts by id
      */
-    deleteContractQuery = gql`
+	deleteContractQuery = gql`
 		mutation delcontracts($Id: Int!) {
 			delcontracts(Id: $Id, IsActive: 0) {
 				Id
@@ -76,95 +81,105 @@ class MainContract extends Component {
 		}
 	`;
 
-    deleteContractById = id => {
-        // this.setState(prevState => ({
-        //     data: this.state.data.filter((_, i) => {
-        //         let element = _.map(item => item.Id);
-        //         return (element !== id);
-        //     })
-        // }), () => {
-        //
-        // });
+	deleteContractById = (id) => {
+		// this.setState(prevState => ({
+		//     data: this.state.data.filter((_, i) => {
+		//         let element = _.map(item => item.Id);
+		//         return (element !== id);
+		//     })
+		// }), () => {
+		//
+		// });
 
-        this.setState({
-            loadingRemoving: true,
-        }, () => {
-            this.props.client
-                .mutate({
-                    mutation: this.deleteContractQuery,
-                    variables: {
-                        Id: id
-                    },
-                })
-                .then(data => {
-                    this.setState({
-                        loadingRemoving: false
-                    })
-                })
-                .catch(error => console.log(error))
-        });
-    };
+		this.setState(
+			{
+				loadingRemoving: true
+			},
+			() => {
+				this.props.client
+					.mutate({
+						mutation: this.deleteContractQuery,
+						variables: {
+							Id: id
+						}
+					})
+					.then((data) => {
+						this.setState({
+							loadingRemoving: false
+						});
+					})
+					.catch((error) => console.log(error));
+			}
+		);
+	};
 
-    render() {
-        // If contracts query is loading, show a progress component
-        if (this.state.loadingContracts) {
-            return <LinearProgress/>
-        }
+	render() {
+		// If contracts query is loading, show a progress component
+		if (this.state.loadingContracts) {
+			return <LinearProgress />;
+		}
 
-        if (this.state.loadingRemoving) {
-            return (
-                <div className="nothing-container">
-                    <CircularProgress size={150}/>
-                </div>
-            )
-        }
+		if (this.state.loadingRemoving) {
+			return (
+				<div className="nothing-container">
+					<CircularProgress size={150} />
+				</div>
+			);
+		}
 
+		// To render the content of the header
+		let renderHeaderContent = () => (
+			<div className="company-list__header">
+				<div className="search-container">
+					<input type="text" placeholder="Search contract" className="input-search-contract" />
+					<button className="button-search-contract">Search</button>
+				</div>
+				<button
+					className="add-company"
+					onClick={() => {
+						this.redirectToCreateContract();
+					}}
+				>
+					Add Contract
+				</button>
+			</div>
+		);
 
-        // To render the content of the header
-        let renderHeaderContent = () => (
-            <div className="company-list__header">
-                <div className="search-container">
-                    <input type="text" placeholder="Search contract" className="input-search-contract"/>
-                    <button className="button-search-contract">Search</button>
-                </div>
-                <button className="add-company" onClick={() => {
-                    this.redirectToCreateContract()
-                }}>Add Contract</button>
-            </div>
-
-        );
-
-        return (
-            <div className="main-contract">
-                <div className="main-contract__header">
-                    {renderHeaderContent()}
-                </div>
-                <div className="main-contract__content">
-                    <Query query={this.getContractsQuery} pollInterval={300}>
-                        {({loading, error, data, refetch, networkStatus}) => {
-                            //if (networkStatus === 4) return <LinearProgress />;
-                            if (loading) return <LinearProgress/>;
-                            if (error) return <p>Error </p>;
-                            if (data.getcontracts != null && data.getcontracts.length > 0) {
-                                return (
-                                    <TablesContracts
-                                        data={data.getcontracts}
-                                        delete={(id) => {
-                                            this.deleteContractById(id)
-                                        }}/>
-                                )
-                            }
-                            return (
-                                <NothingToDisplay
-                                    url="https://cdn3.iconfinder.com/data/icons/business-2-3/256/Contract-512.png"
-                                    message="There are no contracts"/>
-                            )
-                        }}
-                    </Query>
-                </div>
-            </div>
-        );
-    }
+		return (
+			<div className="main-contract-tab">
+				<div className="main-contract__header">{renderHeaderContent()}</div>
+				<div className="main-contract__content">
+					<Query query={this.getContractsQuery} pollInterval={300}>
+						{({ loading, error, data, refetch, networkStatus }) => {
+							//if (networkStatus === 4) return <LinearProgress />;
+							if (loading) return <LinearProgress />;
+							if (error) return <p>Error </p>;
+							if (data.getcontracts != null && data.getcontracts.length > 0) {
+								return (
+									<div className="main-contract-table">
+										<div className="contract_table_wrapper">
+											<TablesContracts
+												data={data.getcontracts}
+												delete={(id) => {
+													this.deleteContractById(id);
+												}}
+											/>
+										</div>
+									</div>
+								);
+							}
+							return (
+								<NothingToDisplay
+									url="https://cdn3.iconfinder.com/data/icons/business-2-3/256/Contract-512.png"
+									message="There are no contracts"
+								/>
+							);
+						}}
+					</Query>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default withApollo(MainContract);
