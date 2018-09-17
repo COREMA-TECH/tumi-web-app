5f359d639b2551cbf2dd38488f97b6a6e699864c
Gershell López <grlopez90@gmail.com>
2018-09-17 15:35:45 -0600

    desapareciendo menu mobil al clicear en uno de los items

--- src/components/Nav/MobileMenu/MenuItem.js
+++ src/components/Nav/MobileMenu/MenuItem.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import Global from 'Generic/Global';
 
 const MenuItem = (props) => (
-	<Link to={`/home/${props.item}`} className="MenuMobile-link" onClick="prop.handleCloseMenu">
+	<Link to={`/home/${props.item}`} className="MenuMobile-link" onClick={props.handleCloseMenu}>
 		<span className={props.icon} title={props.title}></span>
 		<span>{props.title}</span>
 	</Link>
