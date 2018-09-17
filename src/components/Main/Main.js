1ca29c8921011bef3db8ed818b918f85cac0ecbc
LaurenceM10 <laurenmontenegro10@gmail.com>
2018-09-17 16:35:04 -0600

    Fixed error in merge

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,28 +6,22 @@ import Private from '../Private/Private';
 import PrivateRoute from '../PrivateRoute/PrivateRoute';
 
 class Main extends Component {
-	LoginId = sessionStorage.getItem('LoginId');
-	render() {
-<<<<<<< HEAD
-		if (!this.LoginId & (window.location.pathname != '/login')) {
-			window.location.href = '/login';
-			return false;
-=======
-		if (!window.location.pathname.toLocaleLowerCase().startsWith('home/signature')) {
-			if (!this.LoginId & (window.location.pathname != '/login')) {
-				window.location.href = '/login';
-				return false;
-			}
->>>>>>> e405e581056a7cb6a62f5de7ef9af0d95509676d
-		}
+    LoginId = sessionStorage.getItem('LoginId');
+    render() {
+        if (!window.location.pathname.toLocaleLowerCase().startsWith('home/signature')) {
+            if (!this.LoginId & (window.location.pathname != '/login')) {
+                window.location.href = '/login';
+                return false;
+            }
+        }
 
-		return (
-			<div>
-				<Route path="/login" component={Login} />
-				<PrivateRoute path="/home" component={Private} />
-			</div>
-		);
-	}
+        return (
+            <div>
+                <Route path="/login" component={Login} />
+                <PrivateRoute path="/home" component={Private} />
+            </div>
+        );
+    }
 }
 
 export default Main;
