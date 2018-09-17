a2d0e31bc35774cd8bf140106a7ca5b31f3dede3
marpo89 <miltonpomares_13@hotmail.com>
2018-09-17 16:19:37 -0600

    Change in Contracts

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,12 +8,11 @@ import PrivateRoute from '../PrivateRoute/PrivateRoute';
 class Main extends Component {
 	LoginId = sessionStorage.getItem('LoginId');
 	render() {
-		console.log('Login Id', this.LoginId);
-		console.log('Window Locatio Pathname', window.location.pathname);
 		if (!this.LoginId & (window.location.pathname != '/login')) {
 			window.location.href = '/login';
 			return false;
 		}
+
 		return (
 			<div>
 				<Route path="/login" component={Login} />
