ff78ded17968d43dd5048a4a476fc204b3e9d283
Gershell López <grlopez90@gmail.com>
2018-09-20 08:26:17 -0600

    estilos para autosuggest

--- src/components/ui-components/AutosuggestInput/AutosuggestInput.js
+++ src/components/ui-components/AutosuggestInput/AutosuggestInput.js
@@ -12,14 +12,14 @@ class IntegrationAutosuggest extends React.Component {
 			<ReactAutocomplete
 				id={this.props.id}
 				name={this.props.name}
-				inputProps={{ className: this.props.error ? 'input-form _invalid' : 'input-form' }}
+				inputProps={{ className: this.props.error ? 'input-form _invalid input-autocomplete' : 'input-form input-autocomplete' }}
 				items={this.props.data}
 				shouldItemRender={(item, value) => item.Name.toLowerCase().indexOf(value.toLowerCase()) > -1}
 				getItemValue={(item) => {
 					return item.Name.trim();
 				}}
 				renderItem={(item, highlighted) => (
-					<div key={item.Id} style={{ backgroundColor: highlighted ? '#eee' : 'transparent' }}>
+					<div key={item.Id} style={{ backgroundColor: highlighted ? '#eee' : '#FFF' }}>
 						{item.Name.trim()}
 					</div>
 				)}
