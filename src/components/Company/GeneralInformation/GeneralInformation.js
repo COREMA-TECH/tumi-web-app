5ad206a20101a191ef1690f380750b79191b8ad0
Gershell López <grlopez90@gmail.com>
2018-09-20 10:42:46 -0600

    Input file name en edit company

--- src/components/Company/GeneralInformation/GeneralInformation.js
+++ src/components/Company/GeneralInformation/GeneralInformation.js
@@ -1231,7 +1231,7 @@ class GeneralInformation extends Component {
 								/>
 							</div>
 							<div className="card-form-row card-form-row--center">
-								<span className="primary" />
+								<input type="text" class="input-file-name" max="120" placeholder="Name File"/>
 								<FileUpload
 									updateURL={(url) => {
 										this.setState({
@@ -1242,7 +1242,7 @@ class GeneralInformation extends Component {
 								/>
 							</div>
 							<div className="card-form-row card-form-row--center">
-								<span className="primary" />
+								<input type="text" class="input-file-name" max="120" placeholder="Name File"/>
 								<FileUpload
 									updateURL={(url) => {
 										this.setState({
