e38ca869e6ad39baba86249897553da46cbf6041
LaurenceM10 <laurenmontenegro10@gmail.com>
2018-09-21 13:39:19 -0600

    Fixed error in Mutations.js file

--- src/components/ApplyForm/ApplyForm.js
+++ src/components/ApplyForm/ApplyForm.js
@@ -1,65 +1,13 @@
 import React, {Component} from 'react';
 import {Mutation} from "react-apollo";
-import gql from 'graphql-tag';
+import {CREATE_APPLICATION} from "./Mutations";
 
 class ApplyForm extends Component {
     constructor(props) {
         super(props);
     }
 
-
-
     render() {
-        const CREATE_APPLICATION = gql`
-            mutation createApplication(
-                    $firstName: String!,
-                    $middleName: String!,
-                    $lastName: String!,
-                    $date: String!,
-                    $streetAddress: String!,
-                    $aptNumber: Int!,
-                    $city: Int!,
-                    $state: Int!,
-                    $zipCode: Int!,
-                    $homePhone: String!,
-                    $cellPhone: String!,
-                    $socialSecurityNumber: String!,
-                    $emailAddress: String!,
-                    $positionApplyingFor: Int!,
-                    $dateAvailable: String!,
-                    $scheduleRestrictions: Int!,
-                    $scheduleExplain: String,
-                    $convicted: Int!,
-                    $convictedExplain: String,
-                    $comment: String!
-            ) {
-                createApplication(
-                    firstName: $firstName
-                    middleName: $middleName
-                    lastName: $lastName
-                    date: $date
-                    streetAddress: $streetAddress
-                    aptNumber: $aptNumber
-                    city: $city
-                    state: $state
-                    zipCode: $zipCode
-                    homePhone: $homePhone
-                    cellPhone: $cellPhone
-                    socialSecurityNumber: $socialSecurityNumber
-                    emailAddress: $emailAddress
-                    positionApplyingFor: $positionApplyingFor
-                    dateAvailable: $dateAvailable
-                    scheduleRestrictions: $scheduleRestrictions
-                    scheduleExplain: $scheduleExplain
-                    convicted: $convicted
-                    convictedExplain: $convictedExplain
-                    comment: $comment
-                ) {
-                    id
-                }
-            }
-        `;
-
 
         return (
             <Mutation mutation={CREATE_APPLICATION}>
