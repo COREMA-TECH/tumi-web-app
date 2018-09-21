e6aa33bfec08bf4d3a399826c650e15f4a2a67d1
LaurenceM10 <laurenmontenegro10@gmail.com>
2018-09-21 14:53:00 -0600

    Added query to get all positions in ApplyForm component

--- src/components/ApplyForm/Queries.js
+++ src/components/ApplyForm/Queries.js
@@ -30,10 +30,14 @@ export const GET_CITIES_QUERY = gql`
  * Query to get positions
  */
 export const GET_POSITIONS_QUERY = gql`
-    query Cities($parent: Int!) {
-        getcatalogitem(Id: null, IsActive: 1, Id_Parent: $parent, Id_Catalog: 5) {
+    {
+        getposition(IsActive: 1, Id_Entity: null) {
             Id
-            Name
+            Id_Department
+            Position
+            Bill_Rate
+            Pay_Rate
+            Shift
             IsActive
         }
     }
