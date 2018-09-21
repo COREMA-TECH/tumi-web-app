bea9335c1cf656d743024b28a69834af59bfd648
LaurenceM10 <laurenmontenegro10@gmail.com>
2018-09-21 08:49:00 -0600

    Added ApplyForm component

---
+++ src/components/ApplyForm/ApplyForm.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class ApplyForm extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+
+export default ApplyForm;
