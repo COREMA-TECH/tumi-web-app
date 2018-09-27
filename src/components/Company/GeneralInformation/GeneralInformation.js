8b4177346e57dc4af9b2d06c3961e9e75418675c
Gershell <grlopez90@gmail.com>
2018-09-27 15:16:33 -0600

    cambios powerpoint

--- src/components/Company/GeneralInformation/GeneralInformation.js
+++ src/components/Company/GeneralInformation/GeneralInformation.js
@@ -162,7 +162,7 @@ class GeneralInformation extends Component {
 		}
 	`;
 
-	loadCompany = (func = () => {}) => {
+	loadCompany = (func = () => { }) => {
 		this.setState(
 			{
 				loading: true
@@ -233,7 +233,7 @@ class GeneralInformation extends Component {
 		);
 	};
 
-	loadCompanyProperties = (func = () => {}) => {
+	loadCompanyProperties = (func = () => { }) => {
 		this.setState(
 			{
 				loadingCompanyProperties: true
@@ -505,7 +505,7 @@ class GeneralInformation extends Component {
      *  MUTATION TO CREATE COMPANIES WITH GENERAL INFORMATION  *
      **********************************************************/
 
-	loadCountries = (func = () => {}) => {
+	loadCountries = (func = () => { }) => {
 		this.setState({
 			loadingCountries: true
 		});
@@ -543,7 +543,7 @@ class GeneralInformation extends Component {
 			});
 	};
 
-	loadStates = (func = () => {}) => {
+	loadStates = (func = () => { }) => {
 		this.setState({
 			loadingStates: true
 		});
@@ -585,7 +585,7 @@ class GeneralInformation extends Component {
 			});
 	};
 
-	loadCities = (func = () => {}) => {
+	loadCities = (func = () => { }) => {
 		this.setState({
 			loadingCities: true
 		});
@@ -726,7 +726,7 @@ class GeneralInformation extends Component {
 			Code: '',
 			Code01: '',
 			active: 0,
-			suite: 0,
+			suite: ' ',
 			dialogTabValue: 1,
 			propertyClick: false,
 			idProperty: 0,
@@ -985,7 +985,7 @@ class GeneralInformation extends Component {
 		);
 	}
 
-	validateForm(func = () => {}) {
+	validateForm(func = () => { }) {
 		this.setState(
 			{
 				formValid:
@@ -1099,8 +1099,8 @@ class GeneralInformation extends Component {
 						)}
 					</div>
 				) : (
-					''
-				)}
+						''
+					)}
 				<div className="general-information__content">
 					<div className="card-form-company">
 						<div className="card-form-header grey">General Information</div>
@@ -1438,8 +1438,8 @@ class GeneralInformation extends Component {
 						)}
 					</div>
 				) : (
-					''
-				)}
+						''
+					)}
 
 				<Dialog
 					open={this.state.open}
@@ -1470,15 +1470,15 @@ class GeneralInformation extends Component {
 								handleOpenSnackbar={this.props.handleOpenSnackbar}
 							/>
 						) : (
-							//Si el click no es en esa property : pasar el Id en nulo
-							//para que no cargue niguna información relacionada con ese Id
-							<TabsInDialog
-								idCompany={this.props.idCompany}
-								Markup={this.state.rate}
-								handleClose={this.handleClose}
-								handleOpenSnackbar={this.props.handleOpenSnackbar}
-							/>
-						)}
+								//Si el click no es en esa property : pasar el Id en nulo
+								//para que no cargue niguna información relacionada con ese Id
+								<TabsInDialog
+									idCompany={this.props.idCompany}
+									Markup={this.state.rate}
+									handleClose={this.handleClose}
+									handleOpenSnackbar={this.props.handleOpenSnackbar}
+								/>
+							)}
 					</DialogContent>
 				</Dialog>
 			</div>
