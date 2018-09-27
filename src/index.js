19e3f7fd5b173757279423491fcc743128b4f885
Olonyl Landeros <jose87.landeros@gmail.com>
2018-09-26 23:39:37 -0600

    Changed index client

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ import firebase from 'firebase';
 
 // Initialize firebase with config
 firebase.initializeApp({
-    apiKey: 'AIzaSyBvha5XSztYQjUYvnh-THlS6WKEC2MPkbg',
-    authDomain: 'tumiapp-66cd6.firebaseapp.com',
-    databaseURL: 'https://tumiapp-66cd6.firebaseio.com',
-    projectId: 'tumiapp-66cd6',
-    storageBucket: 'tumiapp-66cd6.appspot.com',
-    messagingSenderId: '79324693031'
+	apiKey: 'AIzaSyBvha5XSztYQjUYvnh-THlS6WKEC2MPkbg',
+	authDomain: 'tumiapp-66cd6.firebaseapp.com',
+	databaseURL: 'https://tumiapp-66cd6.firebaseio.com',
+	projectId: 'tumiapp-66cd6',
+	storageBucket: 'tumiapp-66cd6.appspot.com',
+	messagingSenderId: '79324693031'
 });
 
 /**
@@ -23,15 +23,15 @@ firebase.initializeApp({
  */
 // Endpoint URL
 const httpLink = createHttpLink({
-    //uri: 'https://morning-lake-18657.herokuapp.com/graphql'
-    // uri: 'https://corema-new-api.herokuapp.com/graphql'
-    uri: 'http://192.168.0.108:4000/graphql'
+	//uri: 'https://morning-lake-18657.herokuapp.com/graphql'
+	uri: 'https://corema-new-api.herokuapp.com/graphql'
+	//uri: 'http://192.168.0.108:4000/graphql'
 });
 
 // To configure Apollo client with link (url) endpoint and cache option
 const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache()
+	link: httpLink,
+	cache: new InMemoryCache()
 });
 
 /**
@@ -39,8 +39,8 @@ const client = new ApolloClient({
  * that gets passed the client as a prop.
  */
 ReactDOM.render(
-    <ApolloProvider client={client}>
-        <App />
-    </ApolloProvider>,
-    document.getElementById('root')
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
 );
