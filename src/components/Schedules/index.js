6c4331d6b090b2206748d89131969eb64edcb334
Gershell López <grlopez90@gmail.com>
2018-12-13 11:10:07 -0600

    first commit schedules

---
+++ src/components/Schedules/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Schedules extends Component {
+
+    render() {
+        return (
+            <div className="MasterSchedule"></div>
+        );
+    }
+
+}
+
+export default Schedules;
