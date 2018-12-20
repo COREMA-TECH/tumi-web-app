f99ff90084812226ba3c7ba634c0a66a99b301c1
marpo89 <miltonpomares_13@hotmail.com>
2018-12-20 13:15:36 -0600

    Homologar vistas de las work order y los openings

--- src/components/WorkOrdersPosition/WorkOrdersPositionForm.js
+++ src/components/WorkOrdersPosition/WorkOrdersPositionForm.js
@@ -6,29 +6,40 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import withMobileDialog from '@material-ui/core/withMobileDialog';
 import { withApollo } from 'react-apollo';
-import { GET_HOTEL_QUERY, GET_POSITION_BY_QUERY } from './queries';
+import { GET_HOTEL_QUERY, GET_POSITION_BY_QUERY, GET_RECRUITER, GET_CONTACT_BY_QUERY } from './queries';
 import { CREATE_WORKORDER, UPDATE_WORKORDER } from './mutations';
-import ShiftsData from '../../data/shitfs.json';
+import ShiftsData from '../../data/shitfsWorkOrder.json';
+//import ShiftsData from '../../data/shitfs.json';
+import { parse } from 'path';
+import { bool } from 'prop-types';
+import AutosuggestInput from 'ui-components/AutosuggestInput/AutosuggestInput';
+import TimeField from 'react-simple-timefield';
 
 class WorkOrdersPositionForm extends Component {
 	_states = {
 		id: null,
 		hotel: 0,
 		IdEntity: null,
-		date: '',
+		date: new Date().toISOString().substring(0, 10),
 		quantity: 0,
 		status: 1,
 		shift: '',
+		endShift: '',
 		startDate: '',
 		endDate: '',
 		needExperience: false,
 		needEnglish: false,
 		comment: '',
+		EspecialComment: '',
 		position: 0,
 		PositionRateId: null,
-		userId: 1,
+		RecruiterId: null,
+		contactId: null,
+		userId: localStorage.getItem('LoginId'),
 		ShiftsData: ShiftsData,
-		saving: false
+		saving: false,
+		isAdmin: Boolean(localStorage.getItem('IsAdmin'))
+
 	};
 
 	constructor(props) {
@@ -37,6 +48,8 @@ class WorkOrdersPositionForm extends Component {
 			openModal: false,
 			hotels: [],
 			positions: [],
+			recruiters: [],
+			contacts: [],
 			...this._states
 		};
 	}
@@ -48,38 +61,49 @@ class WorkOrdersPositionForm extends Component {
 			this.setState(
 				{
 					id: nextProps.item.id,
+					contactId: nextProps.item.contactId,
 					IdEntity: nextProps.item.IdEntity,
 					date: nextProps.item.date,
 					quantity: nextProps.item.quantity,
-					status: 2,
+					status: nextProps.item.status,
 					shift: nextProps.item.shift,
+					endShift: nextProps.item.endShift,
 					startDate: nextProps.item.startDate,
 					endDate: nextProps.item.endDate,
 					needExperience: nextProps.item.needExperience,
 					needEnglish: nextProps.item.needEnglish,
 					comment: nextProps.item.comment,
-					userId: 1,
-					openModal: nextProps.openModal
+					userId: localStorage.getItem('LoginId'),
+					openModal: nextProps.openModal,
+					EspecialComment: nextProps.item.EspecialComment
+					//isAdmin: Boolean(localStorage.getItem('IsAdmin'))
 				},
 				() => {
 					this.getPositions(nextProps.item.IdEntity, nextProps.item.PositionRateId);
+					this.getContacts(nextProps.item.IdEntity);
+					this.getRecruiter();
+
 					this.ReceiveStatus = true;
 				}
 			);
 		} else if (!this.state.openModal) {
 			this.setState({
 				IdEntity: 0,
-				date: '',
+				date: new Date().toISOString().substring(0, 10),
 				quantity: 0,
 				status: 0,
 				shift: '',
+				endShift: '',
 				startDate: '',
 				endDate: '',
 				needExperience: false,
 				needEnglish: false,
 				comment: '',
+				EspecialComment: '',
 				PositionRateId: 0,
-				userId: 1
+				contactId: 0,
+				userId: localStorage.getItem('LoginId'),
+				isAdmin: Boolean(localStorage.getItem('IsAdmin'))
 			});
 		}
 		this.setState({
@@ -88,6 +112,7 @@ class WorkOrdersPositionForm extends Component {
 	}
 
 	UNSAFE_componentWillMount() {
+
 		this.props.client
 			.query({
 				query: GET_HOTEL_QUERY
@@ -101,12 +126,12 @@ class WorkOrdersPositionForm extends Component {
 
 		this.setState({
 			openModal: this.props.openModal
+
 		});
 	}
 
 	handleSubmit = (event) => {
 		event.preventDefault();
-
 		if (
 			this.state.IdEntity == 0 ||
 			this.state.PositionRateId == 0 ||
@@ -116,8 +141,12 @@ class WorkOrdersPositionForm extends Component {
 			this.state.startDate == '' ||
 			this.state.endDate == '' ||
 			this.state.shift == '' ||
-			this.state.shift == 0
+			this.state.shift == 0 ||
+			this.state.endShift == '' ||
+			this.state.endShift == 0 ||
+			this.state.contactId == ''
 		) {
+
 			this.props.handleOpenSnackbar('error', 'Error all fields are required');
 		} else {
 			this.setState({ saving: true });
@@ -133,17 +162,21 @@ class WorkOrdersPositionForm extends Component {
 				variables: {
 					workOrder: {
 						IdEntity: this.state.IdEntity,
+						//contactId: this.state.contactId
 						date: this.state.date,
 						quantity: this.state.quantity,
-						status: 30452,
+						status: 1,
 						shift: this.state.shift,
+						endShift: this.state.endShift,
 						startDate: this.state.startDate,
 						endDate: this.state.endDate,
 						needExperience: this.state.needExperience,
 						needEnglish: this.state.needEnglish,
 						comment: this.state.comment,
+						EspecialComment: this.state.EspecialComment,
 						PositionRateId: this.state.PositionRateId,
-						userId: 1
+						contactId: this.state.contactId,
+						userId: this.state.userId
 					}
 				}
 			})
@@ -153,12 +186,12 @@ class WorkOrdersPositionForm extends Component {
 				window.location.reload();
 			})
 			.catch((error) => {
+				this.setState({ saving: true });
 				this.props.handleOpenSnackbar('error', 'Error: ' + error);
-				this.setState({ saving: false });
 			});
 	};
 
-	update = (status = 1) => {
+	update = (status = 2) => {
 		this.props.client
 			.mutate({
 				mutation: UPDATE_WORKORDER,
@@ -168,58 +201,94 @@ class WorkOrdersPositionForm extends Component {
 						IdEntity: this.state.IdEntity,
 						date: this.state.date,
 						quantity: this.state.quantity,
-						status: this.state.status,
+						status: status,
 						shift: this.state.shift,
+						endShift: this.state.endShift,
 						startDate: this.state.startDate,
 						endDate: this.state.endDate,
 						needExperience: this.state.needExperience,
 						needEnglish: this.state.needEnglish,
 						comment: this.state.comment,
+						EspecialComment: this.state.EspecialComment,
 						PositionRateId: this.state.PositionRateId,
-						userId: localStorage.getItem('LoginId')
+						userId: this.state.userId,
+						contactId: this.state.contactId
 					}
 				}
 			})
 			.then((data) => {
 				this.props.handleOpenSnackbar('success', 'Record Updated!');
-				this.setState({ openModal: false });
+				this.setState({ openModal: false, saving: false, converting: false });
 				window.location.reload();
 			})
 			.catch((error) => {
-				this.setState({ saving: false });
+				this.setState({ saving: true, converting: false });
 				this.props.handleOpenSnackbar('error', 'Error: ' + error);
 			});
 	};
 
-	handleChangeState = () => {
+	getContacts = (id) => {
+		this.props.client
+			.query({
+				query: GET_CONTACT_BY_QUERY,
+				variables: { id: id }
+			})
+			.then(({ data }) => {
+				this.setState({
+					contacts: data.getcontacts
+				});
+			})
+			.catch();
+	};
+
+	handleChangeState = (event) => {
+		event.preventDefault();
 		if (
 			this.state.IdEntity == 0 ||
 			this.state.PositionRateId == 0 ||
+			this.state.contactId == 0 ||
 			this.state.quantity == '' ||
 			this.state.quantity == 0 ||
 			this.state.date == '' ||
 			this.state.startDate == '' ||
 			this.state.endDate == '' ||
 			this.state.shift == '' ||
-			this.state.shift == 0
+			this.state.shift == 0 ||
+			this.state.endShift == '' ||
+			this.state.endShift == 0
 		) {
 			this.props.handleOpenSnackbar('error', 'Error all fields are required');
 		} else {
-			this.update(2);
+			this.setState({ converting: true });
+			// this.update(2);
+			this.CONVERT_TO_OPENING();
 		}
 	};
 
+
+
 	handleChange = (event) => {
+		console.log("veamos el evento ", event);
 		const target = event.target;
 		const value = target.type === 'checkbox' ? target.checked : target.value;
 		const name = target.name;
+		let comments = '';
 
+		// console.log("veamos el evento target ", target, " value ", value, " name ", name);
 		this.setState({
 			[name]: value
 		});
 
+		if (name === 'PositionRateId') {
+			comments = this.state.positions.find((item) => { return item.Id == value })
+			this.setState({
+				EspecialComment: comments != null ? comments.Comment : ''
+			});
+		}
+
 		if (name === 'IdEntity') {
 			this.getPositions(value);
+			this.getContacts(value);
 		}
 	};
 
@@ -230,21 +299,67 @@ class WorkOrdersPositionForm extends Component {
 				variables: { id: id }
 			})
 			.then(({ data }) => {
+				console.log("positions ", data.getposition)
 				this.setState({
 					positions: data.getposition,
 					PositionRateId: PositionId
+					//EspecialComment: data.getposition[0].Comment
 				});
 			})
 			.catch();
 	};
 
+	getRecruiter = () => {
+		this.props.client
+			.query({
+				query: GET_RECRUITER,
+				variables: {}
+			})
+			.then(({ data }) => {
+				this.setState({
+					recruiters: data.getusers
+
+				});
+			})
+			.catch();
+	};
+
+	validateInvalidInput = () => {
+		if (document.addEventListener) {
+			document.addEventListener(
+				'invalid',
+				(e) => {
+					e.target.className += ' invalid-apply-form';
+				},
+				true
+			);
+		}
+	};
+
+	handleTimeChange = (name) => (text) => {
+		this.setState({ [name]: text })
+	}
+
+	handleValidate = (event) => {
+		let selfHtml = event.currentTarget;
+		if (selfHtml.value == "" || selfHtml.value == 0)
+			selfHtml.classList.add("is-invalid");
+		else
+			selfHtml.classList.remove("is-invalid");
+
+	}
+
 	render() {
+
+		const isAdmin = localStorage.getItem('IsAdmin') == "true"
+
 		return (
+
 			<div>
 				<Dialog maxWidth="md" open={this.state.openModal} onClose={this.props.handleCloseModal}>
 					<DialogTitle style={{ padding: '0px' }}>
 						<div className="modal-header">
-							<h5 className="modal-title">Opening</h5>
+							<h5 className="modal-title">Work Order</h5>
 						</div>
 					</DialogTitle>
 					<DialogContent>
@@ -255,11 +370,14 @@ class WorkOrdersPositionForm extends Component {
 										<div className="col-md-6">
 											<label htmlFor="">Hotel</label>
 											<select
+												required
 												name="IdEntity"
 												className="form-control"
 												id=""
 												onChange={this.handleChange}
 												value={this.state.IdEntity}
+												disabled={!isAdmin}
+												onBlur={this.handleValidate}
 											>
 												<option value={0}>Select a Hotel</option>
 												{this.state.hotels.map((hotel) => (
@@ -268,13 +386,33 @@ class WorkOrdersPositionForm extends Component {
 											</select>
 										</div>
 										<div className="col-md-6">
+											<label htmlFor="">Requested by</label>
+											<select
+												required
+												name="contactId"
+												className="form-control"
+												id=""
+												onChange={this.handleChange}
+												value={this.state.contactId}
+												disabled={!isAdmin}
+												onBlur={this.handleValidate}
+											>
+												<option value={0}>Select a Contact</option>
+												{this.state.contacts.map((contact) => (
+													<option value={contact.Id}>{contact.First_Name + contact.Last_Name}</option>
+												))}
+											</select>
+										</div>
+										<div className="col-md-6">
 											<label htmlFor="">Position</label>
 											<select
+												required
 												name="PositionRateId"
 												className="form-control"
 												id=""
 												onChange={this.handleChange}
 												value={this.state.PositionRateId}
+												onBlur={this.handleValidate}
 											>
 												<option value="0">Select a Position</option>
 												{this.state.positions.map((position) => (
@@ -283,65 +421,92 @@ class WorkOrdersPositionForm extends Component {
 											</select>
 										</div>
 										<div className="col-md-6">
-											<label htmlFor="">Quantity</label>
+											<label htmlFor="">Requested on Date</label>
 											<input
-												type="number"
+												required
+												type="date"
 												className="form-control"
-												name="quantity"
+												name="date"
+												disabled={true}
 												onChange={this.handleChange}
-												value={this.state.quantity}
+												value={this.state.date.substring(0, 10)}
+												onBlur={this.handleValidate}
 											/>
 										</div>
 										<div className="col-md-6">
-											<label htmlFor="">Shift</label>
-											<select
-												className="form-control"
-												name="shift"
-												onChange={this.handleChange}
-												value={this.state.shift}
-											>
-												<option value="0">Select a Shift</option>
-												{this.state.ShiftsData.map((shift) => (
-													<option value={shift.Id}>{shift.Name}</option>
-												))}
-											</select>
+											<label htmlFor="">Shift Start</label>
+											<TimeField name="shift" style={{ width: '100%' }} className="form-control" value={this.state.shift} onChange={this.handleTimeChange('shift')} />
+
+
 										</div>
 										<div className="col-md-6">
-											<label htmlFor="">Date Needed By</label>
+											<label htmlFor="">Shift End</label>
+											<TimeField name="endShift" style={{ width: '100%' }} className="form-control" value={this.state.endShift} onChange={this.handleTimeChange('endShift')} />
+
+										</div>
+										<div className="col-md-6">
+											<label htmlFor="">From Date</label>
 											<input
+												required
 												type="date"
 												className="form-control"
 												name="startDate"
 												onChange={this.handleChange}
 												value={this.state.startDate.substring(0, 10)}
+												onBlur={this.handleValidate}
 											/>
 										</div>
 										<div className="col-md-6">
-											<label htmlFor="">To</label>
+											<label htmlFor="">To Date</label>
 											<input
+												required
 												type="date"
 												className="form-control"
 												name="endDate"
 												onChange={this.handleChange}
 												value={this.state.endDate.substring(0, 10)}
+												onBlur={this.handleValidate}
 											/>
 										</div>
 										<div className="col-md-6">
-											<label htmlFor="">Date</label>
+											<label htmlFor="">Quantity</label>
 											<input
-												type="date"
+												required
+												type="number"
+												maxLength="10"
+												min={0}
 												className="form-control"
-												name="date"
+												name="quantity"
 												onChange={this.handleChange}
-												value={this.state.date.substring(0, 10)}
+												value={this.state.quantity}
+												onBlur={this.handleValidate}
 											/>
 										</div>
+										{/*this.state.id && (
+											<div className="col-md-6">
+												<label htmlFor="">Assign to</label>
+												<select
+													required
+													name="RecruiterId"
+													className="form-control"
+													id=""
+													onChange={this.handleChange}
+													value={this.state.RecruiterId}
+													onBlur={this.handleValidate}
+												>
+													<option value="0">Select a Recruiter</option>
+													{this.state.recruiters.map((recruiter) => (
+														< option value={recruiter.Id} > {recruiter.Full_Name}</option>
+													))}
+												</select>
+											</div>
+													)*/}
 									</div>
 								</div>
 								<div className="col-md-5 col-5">
 									<div className="row">
 										<div className="col-md-5">
-											<label>Need Experience?</label>
+											<label>Needs Experience?</label>
 											<div className="onoffswitch">
 												<input
 													type="checkbox"
@@ -359,7 +524,7 @@ class WorkOrdersPositionForm extends Component {
 											</div>
 										</div>
 										<div className="col-md-7">
-											<label>Need to Speak English?</label>
+											<label>Needs to Speak English?</label>
 											<div className="onoffswitch">
 												<input
 													type="checkbox"
@@ -378,40 +543,69 @@ class WorkOrdersPositionForm extends Component {
 										</div>
 
 										<div className="col-md-12">
-											<label htmlFor="">Comment</label>
+											<label htmlFor="">Comments</label>
 											<textarea
 												onChange={this.handleChange}
 												name="comment"
 												className="form-control"
 												id=""
 												cols="30"
-												rows="10"
+												rows="3"
 												value={this.state.comment}
+											/>
+										</div>
+										<div className="col-md-12">
+											<label htmlFor="">Special Comments</label>
+											<textarea
+												onChange={this.handleChange}
+												name="EspecialComment"
+												className="form-control"
+												id=""
+												cols="30"
+												rows="3 "
+												disabled={true}
+												value={this.state.EspecialComment}
 											/>
 										</div>
 									</div>
 								</div>
 								<div className="col-md-12">
-									<div className="mt-2">
-										<button
-											className="btn btn-danger ml-1 float-right"
-											onClick={this.props.handleCloseModal}
-										>
-											Cancel<i class="fas fa-ban ml-2" />
-										</button>
-										<button className="btn btn-success ml-1 float-right" type="submit">
-											Save {!this.state.saving && <i class="fas fa-save ml2" />}
-											{this.state.saving && <i class="fas fa-spinner fa-spin  ml2" />}
-										</button>
+									<div className="row">
+										<div className="col-md-6">
+
+										</div>
+										<div className="col-md-12">
+											<button
+												className="btn btn-danger ml-1 float-right"
+												onClick={this.props.handleCloseModal}
+											>
+												Cancel<i class="fas fa-ban ml-2" />
+											</button>
+											<button className="btn btn-success ml-1 float-right" type="submit">
+												Save {!this.state.saving && <i class="fas fa-save ml2" />}
+												{this.state.saving && <i class="fas fa-spinner fa-spin  ml2" />}
+											</button>
+											{/*this.state.id && (
+												<button
+													className="btn btn-info float-right"
+													type="button"
+													onClick={this.handleChangeState}
+												>
+													Convert to Opening {!this.state.converting && <i class="fas fa-sync-alt"></i>}
+													{this.state.converting && <i class="fas fa-spinner fa-spin  ml2" />}
+												</button>
+											)*/}
+										</div>
 									</div>
 								</div>
 							</div>
 						</form>
 					</DialogContent>
 				</Dialog>
-			</div>
+			</div >
 		);
 	}
 }
 
 export default (withMobileDialog()(withApollo(WorkOrdersPositionForm)));
+
