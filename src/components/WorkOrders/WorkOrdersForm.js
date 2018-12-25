c5753722dcfb154780a584f17a65f3cd1fa40f40
marpo89 <miltonpomares_13@hotmail.com>
2018-12-25 16:45:53 -0600

    cambios en work order

--- src/components/WorkOrders/WorkOrdersForm.js
+++ src/components/WorkOrders/WorkOrdersForm.js
@@ -14,6 +14,44 @@ import { parse } from 'path';
 import { bool } from 'prop-types';
 import AutosuggestInput from 'ui-components/AutosuggestInput/AutosuggestInput';
 import TimeField from 'react-simple-timefield';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const styles = (theme) => ({
+    wrapper: {
+        margin: theme.spacing.unit,
+        position: 'relative'
+    },
+    buttonSuccess: {},
+    buttonProgress: {
+        //color: ,
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -12,
+        marginLeft: -12
+    },
+    row: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.background.default
+        },
+        '&:hover': {
+            cursor: 'pointer'
+        }
+    }
+});
+
+const CustomTableCell = withStyles((theme) => ({
+    head: {
+        color: theme.palette.common.white
+    },
+    body: {
+        fontSize: 14
+    }
+}))(TableCell);
 
 class WorkOrdersForm extends Component {
     _states = {
@@ -396,7 +434,7 @@ class WorkOrdersForm extends Component {
     }
 
     render() {
-
+        const { classes } = this.props;
         const isAdmin = localStorage.getItem('IsAdmin') == "true"
 
         return (
@@ -482,42 +520,10 @@ class WorkOrdersForm extends Component {
                                         <div className="col-md-6">
                                             <label htmlFor="">Shift Start</label>
                                             <TimeField name="shift" style={{ width: '100%' }} className="form-control" value={this.state.shift} onChange={this.handleTimeChange('shift')} />
-                                            {/* 
-                                            <select
-                                                required
-                                                className="form-control"
-                                                name="shift"
-                                                onChange={this.handleChange}
-                                                value={this.state.shift}
-                                                onBlur={this.handleValidate}
-                                            >
-                                                <option value="0">Select a Shift</option>
-                                                {this.state.ShiftsData.map((shift) => (
-                                                    <option value={shift.Id}>{shift.Name}</option>
-                                                ))}
-                                            </select>
-                                            */}
-
                                         </div>
                                         <div className="col-md-6">
                                             <label htmlFor="">Shift End</label>
                                             <TimeField name="endShift" style={{ width: '100%' }} className="form-control" value={this.state.endShift} onChange={this.handleTimeChange('endShift')} />
-                                            {/*
-                                        <select
-                                                required
-                                                className="form-control"
-                                                name="endShift"
-                                                onChange={this.handleChange}
-                                                value={this.state.endShift}
-                                                onBlur={this.handleValidate}
-                                            >
-                                                <option value="0">Select a Shift End</option>
-                                                {this.state.ShiftsData.map((shift) => (
-                                                    <option value={shift.Id}>{shift.Name}</option>
-                                                ))}
-                                            </select>
-                                        */}
-
                                         </div>
                                         <div className="col-md-6">
                                             <label htmlFor="">From Date</label>
@@ -644,38 +650,45 @@ class WorkOrdersForm extends Component {
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+
+                            <div className='row'>
+                                <div className="col-md-12">
+                                    {this.state.id && (
+                                        <div class="card">
+                                            <div class="card-header danger">Employees assign to work order</div>
+                                            <div class="card-body">
+                                                <Table className="Table">
+                                                    <TableHead>
+                                                        <TableRow>
+                                                            <CustomTableCell className={'Table-head'}>Delete</CustomTableCell>
+                                                            <CustomTableCell className={'Table-head'}>Employees</CustomTableCell>
+                                                        </TableRow>
+                                                    </TableHead>
+                                                    <TableBody>
+                                                        {/*this.state.companyProperties.map((item) => {
+                                                                return (
+                                                                    <TableRow
+                                                                        hover
+                                                                        className={classes.row}
+                                                                        key={item.Id}
+                                                                        onClick={this.handleClickOpen('paper', true, item.Id, item.rate)}
+                                                                    >
+                                                                        <CustomTableCell>{item.Code}</CustomTableCell>
+                                                                        <CustomTableCell>{item.Name}</CustomTableCell>
+                                                                    </TableRow>
+                                                                );
+                                                            })
+                                                        */}
+                                                    </TableBody>
+                                                </Table>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+
                                 <div className="col-md-12">
                                     <div className="row">
-                                        <div className="col-md-6">
-                                            {/*this.state.id && (
-                                                <div class="input-group">
-                                                    <select
-                                                        required
-                                                        name="RecruiterId"
-                                                        className="form-control"
-                                                        id=""
-                                                        onChange={this.handleChange}
-                                                        value={this.state.RecruiterId}
-                                                        onBlur={this.handleValidate}
-                                                    >
-                                                        <option value="0">Select a Recruiter</option>
-                                                        {this.state.recruiters.map((recruiter) => (
-                                                            < option value={recruiter.Id} > {recruiter.Full_Name}</option>
-                                                        ))}
-                                                    </select>
-
-                                                    <div class="input-group-append">
-                                                        <button
-                                                            className="btn btn-info float-right"
-                                                            type="button"
-                                                            onClick={this.handleChangeState}
-                                                        >
-                                                            Create Opening
-														</button>
-                                                    </div>
-                                                </div>
-                                                        )*/}
-                                        </div>
                                         <div className="col-md-12">
                                             <button
                                                 className="btn btn-danger ml-1 float-right"
