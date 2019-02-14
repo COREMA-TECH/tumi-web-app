3419e6c8041aba6bba31bb0e4dc1c7876565e48c
Olonyl Horacio Rocha Landeros <jose87.landeros@gmail.com>
2019-02-14 14:45:54 -0600

    endpoint changed to 5000

--- src/components/connection.js
+++ src/components/connection.js
@@ -2,6 +2,6 @@
 //const baseEndpointURL = 'https://corema-new-api.herokuapp.com';
 //const baseEndpointURL = 'https://tumicerti.herokuapp.com';
 //const baseEndpointURL = 'http://ec2-18-221-184-184.us-east-2.compute.amazonaws.com:4001';
-const baseEndpointURL = 'http://localhost:4000';
+const baseEndpointURL = 'http://localhost:5000';
 
 export const connection = baseEndpointURL;
