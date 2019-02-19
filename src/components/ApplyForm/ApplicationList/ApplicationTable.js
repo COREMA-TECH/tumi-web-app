0ccb100466ce73bc33ae9330edaf8717642c578b
LaurenceM10 <laurenmontenegro10@gmail.com>
2019-02-19 11:54:21 -0600

    Added query to show application status

--- src/components/ApplyForm/ApplicationList/ApplicationTable.js
+++ src/components/ApplyForm/ApplicationList/ApplicationTable.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import {withStyles} from '@material-ui/core/styles';
 import TableCell from '@material-ui/core/TableCell';
 import IconButton from '@material-ui/core/IconButton';
 import LastPageIcon from '@material-ui/icons/LastPage';
@@ -17,255 +17,280 @@ import TablePagination from '@material-ui/core/TablePagination/TablePagination';
 import Paper from '@material-ui/core/Paper/Paper';
 import LinearProgress from '@material-ui/core/LinearProgress/LinearProgress';
 import withApollo from 'react-apollo/withApollo';
-import DeleteIcon from '@material-ui/icons/Delete';
 import Tooltip from '@material-ui/core/Tooltip';
+import {GET_COMPLETED_STATUS} from "./Queries";
 
 const uuidv4 = require('uuid/v4');
 const actionsStyles = (theme) => ({
-	root: {
-		flexShrink: 0,
-		color: theme.palette.text.secondary,
-		marginLeft: theme.spacing.unit * 2.5
-	},
-	paddingDefault: {
-		padding: '40px 24px 40px 16px'
-	}
+    root: {
+        flexShrink: 0,
+        color: theme.palette.text.secondary,
+        marginLeft: theme.spacing.unit * 2.5
+    },
+    paddingDefault: {
+        padding: '40px 24px 40px 16px'
+    }
 });
 
 class TablePaginationActions extends React.Component {
-	handleFirstPageButtonClick = (event) => {
-		this.props.onChangePage(event, 0);
-	};
+    handleFirstPageButtonClick = (event) => {
+        this.props.onChangePage(event, 0);
+    };
 
-	handleBackButtonClick = (event) => {
-		this.props.onChangePage(event, this.props.page - 1);
-	};
+    handleBackButtonClick = (event) => {
+        this.props.onChangePage(event, this.props.page - 1);
+    };
 
-	handleNextButtonClick = (event) => {
-		this.props.onChangePage(event, this.props.page + 1);
-	};
+    handleNextButtonClick = (event) => {
+        this.props.onChangePage(event, this.props.page + 1);
+    };
 
-	handleLastPageButtonClick = (event) => {
-		this.props.onChangePage(event, Math.max(0, Math.ceil(this.props.count / this.props.rowsPerPage) - 1));
-	};
+    handleLastPageButtonClick = (event) => {
+        this.props.onChangePage(event, Math.max(0, Math.ceil(this.props.count / this.props.rowsPerPage) - 1));
+    };
 
-	render() {
-		const { classes, count, page, rowsPerPage, theme } = this.props;
+    render() {
+        const {classes, count, page, rowsPerPage, theme} = this.props;
 
-		return (
-			<div className={classes.root}>
-				<IconButton onClick={this.handleFirstPageButtonClick} disabled={page === 0} aria-label="First Page">
-					{theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-				</IconButton>
-				<IconButton onClick={this.handleBackButtonClick} disabled={page === 0} aria-label="Previous Page">
-					{theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-				</IconButton>
-				<IconButton
-					onClick={this.handleNextButtonClick}
-					disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-					aria-label="Next Page"
-				>
-					{theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-				</IconButton>
-				<IconButton
-					onClick={this.handleLastPageButtonClick}
-					disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-					aria-label="Last Page"
-				>
-					{theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-				</IconButton>
-			</div>
-		);
-	}
+        return (
+            <div className={classes.root}>
+                <IconButton onClick={this.handleFirstPageButtonClick} disabled={page === 0} aria-label="First Page">
+                    {theme.direction === 'rtl' ? <LastPageIcon/> : <FirstPageIcon/>}
+                </IconButton>
+                <IconButton onClick={this.handleBackButtonClick} disabled={page === 0} aria-label="Previous Page">
+                    {theme.direction === 'rtl' ? <KeyboardArrowRight/> : <KeyboardArrowLeft/>}
+                </IconButton>
+                <IconButton
+                    onClick={this.handleNextButtonClick}
+                    disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                    aria-label="Next Page"
+                >
+                    {theme.direction === 'rtl' ? <KeyboardArrowLeft/> : <KeyboardArrowRight/>}
+                </IconButton>
+                <IconButton
+                    onClick={this.handleLastPageButtonClick}
+                    disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+                    aria-label="Last Page"
+                >
+                    {theme.direction === 'rtl' ? <FirstPageIcon/> : <LastPageIcon/>}
+                </IconButton>
+            </div>
+        );
+    }
 }
 
 TablePaginationActions.propTypes = {
-	classes: PropTypes.object.isRequired,
-	count: PropTypes.number.isRequired,
-	onChangePage: PropTypes.func.isRequired,
-	page: PropTypes.number.isRequired,
-	rowsPerPage: PropTypes.number.isRequired,
-	theme: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    count: PropTypes.number.isRequired,
+    onChangePage: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+    theme: PropTypes.object.isRequired
 };
 
-const TablePaginationActionsWrapped = withStyles(actionsStyles, { withTheme: true })(TablePaginationActions);
+const TablePaginationActionsWrapped = withStyles(actionsStyles, {withTheme: true})(TablePaginationActions);
 
 let counter = 0;
 
 function createData(name, calories, fat) {
-	counter += 1;
-	return { id: counter, name, calories, fat };
+    counter += 1;
+    return {id: counter, name, calories, fat};
 }
 
 const CustomTableCell = withStyles((theme) => ({
-	head: {
-		backgroundColor: theme.palette.common.black,
-		color: theme.palette.common.white
-	},
-	body: {
-		fontSize: 14
-	}
+    head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white
+    },
+    body: {
+        fontSize: 14
+    }
 }))(TableCell);
 
 const styles = (theme) => ({
-	root: {
-		width: '100%',
-		marginTop: theme.spacing.unit * 3,
-		overflowX: 'auto'
-	},
-	table: {
-		minWidth: 500
-	},
-	tableWrapper: {
-		overflowX: 'auto'
-	},
-	row: {
-		'&:nth-of-type(odd)': {
-			backgroundColor: theme.palette.background.default
-		},
-		'&:hover': {
-			cursor: 'pointer'
-		}
-	},
-	fab: {
-		margin: theme.spacing.unit * 2
-	},
-	absolute: {
-		position: 'absolute',
-		bottom: theme.spacing.unit * 2,
-		right: theme.spacing.unit * 3
-	},
-	th: {
-		backgroundColor: '#3da2c7'
-	}
+    root: {
+        width: '100%',
+        marginTop: theme.spacing.unit * 3,
+        overflowX: 'auto'
+    },
+    table: {
+        minWidth: 500
+    },
+    tableWrapper: {
+        overflowX: 'auto'
+    },
+    row: {
+        '&:nth-of-type(odd)': {
+            backgroundColor: theme.palette.background.default
+        },
+        '&:hover': {
+            cursor: 'pointer'
+        }
+    },
+    fab: {
+        margin: theme.spacing.unit * 2
+    },
+    absolute: {
+        position: 'absolute',
+        bottom: theme.spacing.unit * 2,
+        right: theme.spacing.unit * 3
+    },
+    th: {
+        backgroundColor: '#3da2c7'
+    }
 });
 
 let id = 0;
 
 class ApplicationTable extends React.Component {
-	state = {
-		page: 0,
-		rowsPerPage: 7
-	};
-	handleChangePage = (event, page) => {
-		this.setState({ page });
-	};
+    state = {
+        page: 0,
+        rowsPerPage: 7,
+        completed: false
+    };
+    handleChangePage = (event, page) => {
+        this.setState({page});
+    };
 
-	handleChangeRowsPerPage = (event) => {
-		this.setState({ rowsPerPage: event.target.value });
-	};
+    handleChangeRowsPerPage = (event) => {
+        this.setState({rowsPerPage: event.target.value});
+    };
 
-	shouldComponentUpdate(nextProps, nextState) {
-		if (this.props.data !== nextProps.data || this.props.loading !== nextProps.loading) {
-			return true;
-		}
-		if (
-			this.state.page !== nextState.page ||
-			this.state.rowsPerPage !== nextState.rowsPerPage //||
-			//	this.state.order !== nextState.order ||
-			//this.state.orderBy !== nextState.orderBy
-		) {
-			return true;
-		}
-		return false;
-	}
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.props.data !== nextProps.data || this.props.loading !== nextProps.loading) {
+            return true;
+        }
+        if (
+            this.state.page !== nextState.page ||
+            this.state.rowsPerPage !== nextState.rowsPerPage //||
+        //	this.state.order !== nextState.order ||
+        //this.state.orderBy !== nextState.orderBy
+        ) {
+            return true;
+        }
+        return false;
+    };
 
-	render() {
-		const { classes } = this.props;
-		let items = this.props.data;
-		const { rowsPerPage, page } = this.state;
-		const emptyRows = rowsPerPage - Math.min(rowsPerPage, items.length - page * rowsPerPage);
+    getStatus = (applicationId) => {
+        alert(applicationId);
+        this.props.client
+            .query({
+                query: GET_COMPLETED_STATUS,
+                variables: {
+                    id: applicationId
+                }
+            })
+            .then(data => {
+                this.setState({
+                    completed: data.applicationCompleted
+                })
+            })
+            .catch(error => {
 
-		if (this.state.loadingRemoving) {
-			return <LinearProgress />;
-		}
+            })
+    };
 
-		return (
-			<Route
-				render={({ history }) => (
-					<Paper className={classes.root}>
-						<Table className={classes.table}>
-							<TableHead>
-								<TableRow>
-									<CustomTableCell padding="none" className={"Table-head text-center"} style={{ width: '50px' }}>Actions</CustomTableCell>
-									<CustomTableCell className={"Table-head"}>Work Order</CustomTableCell>
-									<CustomTableCell className={"Table-head"}>Position Applying For</CustomTableCell>
-									<CustomTableCell className={"Table-head"}>Hotel</CustomTableCell>
-									<CustomTableCell className={"Table-head"}>Recruited By</CustomTableCell>
-									<CustomTableCell className={"Table-head"}>Sent to Interview By</CustomTableCell>
-									<CustomTableCell className={"Table-head"}>Full Name</CustomTableCell>
-									<CustomTableCell className={"Table-head"}>Email Address</CustomTableCell>
-									<CustomTableCell className={"Table-head"}>Completed</CustomTableCell>
-								</TableRow>
-							</TableHead>
-							<TableBody>
-								{items.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
-									return (
-										<TableRow
-											hover
-											className={classes.row}
-											key={uuidv4()}
-											onClick={() => {
-												history.push({
-													pathname: '/home/application/info',
-													state: { ApplicationId: row.id }
-												});
-											}}
-										>
-											<CustomTableCell >
-												<Tooltip title="Delete">
-													<button
-														className="btn btn-danger ml-1"
-														disabled={this.props.loading}
-														onClick={(e) => {
-															e.stopPropagation();
+    componentWillMount(){
+        this.getStatus(this.props.data.id)
+    }
 
-															return this.props.onDeleteHandler(row.id);
-														}}
-													>
-														<i className="fas fa-trash"></i>
-													</button>
-												</Tooltip>
-											</CustomTableCell>
-											<CustomTableCell>{row.idWorkOrder ? `000000${row.idWorkOrder}`.slice(-6) : ''}</CustomTableCell>
-											<CustomTableCell>
-												{row.position ? row.position.position.Position.trim() + '(' + row.position.BusinessCompany.Code.trim() + ')' : 'Open Position'}
-											</CustomTableCell>
-											<CustomTableCell>{row.position ? row.position.BusinessCompany.Name : ''}</CustomTableCell>
-											<CustomTableCell>{row.user ? row.user.Full_Name : ''}</CustomTableCell>
-											<CustomTableCell>{row.recruiter ? row.recruiter.Full_Name : ''}</CustomTableCell>
-											<CustomTableCell>{row.firstName + ' ' + row.lastName}</CustomTableCell>
-											<CustomTableCell>{row.emailAddress}</CustomTableCell>
-											<CustomTableCell>{/*TODO: show row.status */}</CustomTableCell>
-										</TableRow>
-									);
-								})}
-							</TableBody>
-							<TableFooter>
-								<TableRow>
-									{items.length > 0 && (
-										<TablePagination
-											colSpan={3}
-											count={items.length}
-											rowsPerPage={rowsPerPage}
-											page={page}
-											onChangePage={this.handleChangePage}
-											onChangeRowsPerPage={this.handleChangeRowsPerPage}
-											ActionsComponent={TablePaginationActionsWrapped}
-										/>
-									)}
-								</TableRow>
-							</TableFooter>
-						</Table>
-					</Paper>
-				)}
-			/>
-		);
-	}
+    render() {
+        const {classes} = this.props;
+        let items = this.props.data;
+        const {rowsPerPage, page} = this.state;
+        const emptyRows = rowsPerPage - Math.min(rowsPerPage, items.length - page * rowsPerPage);
+
+        if (this.state.loadingRemoving) {
+            return <LinearProgress/>;
+        }
+
+        return (
+            <Route
+                render={({history}) => (
+                    <Paper className={classes.root}>
+                        <Table className={classes.table}>
+                            <TableHead>
+                                <TableRow>
+                                    <CustomTableCell padding="none" className={"Table-head text-center"}
+                                                     style={{width: '50px'}}>Actions</CustomTableCell>
+                                    <CustomTableCell className={"Table-head"}>Work Order</CustomTableCell>
+                                    <CustomTableCell className={"Table-head"}>Position Applying For</CustomTableCell>
+                                    <CustomTableCell className={"Table-head"}>Hotel</CustomTableCell>
+                                    <CustomTableCell className={"Table-head"}>Recruited By</CustomTableCell>
+                                    <CustomTableCell className={"Table-head"}>Sent to Interview By</CustomTableCell>
+                                    <CustomTableCell className={"Table-head"}>Full Name</CustomTableCell>
+                                    <CustomTableCell className={"Table-head"}>Email Address</CustomTableCell>
+                                    <CustomTableCell className={"Table-head"}>Completed</CustomTableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {items.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                                    return (
+                                        <TableRow
+                                            hover
+                                            className={classes.row}
+                                            key={uuidv4()}
+                                            onClick={() => {
+                                                history.push({
+                                                    pathname: '/home/application/info',
+                                                    state: {ApplicationId: row.id}
+                                                });
+                                            }}
+                                        >
+                                            <CustomTableCell>
+                                                <Tooltip title="Delete">
+                                                    <button
+                                                        className="btn btn-danger ml-1"
+                                                        disabled={this.props.loading}
+                                                        onClick={(e) => {
+                                                            e.stopPropagation();
+
+                                                            return this.props.onDeleteHandler(row.id);
+                                                        }}
+                                                    >
+                                                        <i className="fas fa-trash"></i>
+                                                    </button>
+                                                </Tooltip>
+                                            </CustomTableCell>
+                                            <CustomTableCell>{row.idWorkOrder ? `000000${row.idWorkOrder}`.slice(-6) : ''}</CustomTableCell>
+                                            <CustomTableCell>
+                                                {row.position ? row.position.position.Position.trim() + '(' + row.position.BusinessCompany.Code.trim() + ')' : 'Open Position'}
+                                            </CustomTableCell>
+                                            <CustomTableCell>{row.position ? row.position.BusinessCompany.Name : ''}</CustomTableCell>
+                                            <CustomTableCell>{row.user ? row.user.Full_Name : ''}</CustomTableCell>
+                                            <CustomTableCell>{row.recruiter ? row.recruiter.Full_Name : ''}</CustomTableCell>
+                                            <CustomTableCell>{row.firstName + ' ' + row.lastName}</CustomTableCell>
+                                            <CustomTableCell>{row.emailAddress}</CustomTableCell>
+                                            <CustomTableCell>{this.state.completed === true ? "YES" : "NO"}</CustomTableCell>
+                                        </TableRow>
+                                    );
+                                })}
+                            </TableBody>
+                            <TableFooter>
+                                <TableRow>
+                                    {items.length > 0 && (
+                                        <TablePagination
+                                            colSpan={3}
+                                            count={items.length}
+                                            rowsPerPage={rowsPerPage}
+                                            page={page}
+                                            onChangePage={this.handleChangePage}
+                                            onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                                            ActionsComponent={TablePaginationActionsWrapped}
+                                        />
+                                    )}
+                                </TableRow>
+                            </TableFooter>
+                        </Table>
+                    </Paper>
+                )}
+            />
+        );
+    }
 }
 
 ApplicationTable.propTypes = {
-	classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(withApollo(ApplicationTable));
