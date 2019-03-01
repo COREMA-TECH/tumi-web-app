249d793eb676dcece4d75b4a60b6d10e0aaf9099
LaurenceM10 <laurenmontenegro10@gmail.com>
2019-03-01 12:12:08 -0600

    Fix: erros with titles

--- src/components/MainContainer/Main/MainContainer.js
+++ src/components/MainContainer/Main/MainContainer.js
@@ -27,7 +27,7 @@ class MainContainer extends Component {
 
 		title = title == 'Company' ? 'Management' : title;
 
-		if(str === "/home/application/info" || "home/application") {
+		if(str === "/home/application/info" || str === "/home/application") {
 			title = 'Employee Package'
 		}
 
@@ -38,11 +38,6 @@ class MainContainer extends Component {
         if(str === "/home/Company") {
             title = 'Management Company'
         }
-
-        if(str === "/home/company/add") {
-            title = 'Add Management Company'
-        }
-
 
 
 
