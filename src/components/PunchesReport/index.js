ef2b0800ccb4f159c0e6c79140e46ae31d6ac4c8
Olonyl Horacio Rocha Landeros <jose87.landeros@gmail.com>
2019-03-09 13:51:43 -0600

    marked employees query , added filters

--- src/components/PunchesReport/index.js
+++ src/components/PunchesReport/index.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
 import Table from './table';
 import Filter from './filter';
-import moment from 'moment';
 
 import LinearProgress from '@material-ui/core/es/LinearProgress/LinearProgress';
 import withApollo from 'react-apollo/withApollo';
-import { GET_REPORT_QUERY } from './queries';
+import { GET_REPORT_QUERY, GET_DEPARTMENTS_QUERY, GET_PROPERTIES_QUERY } from './queries';
 
 class PunchesReport extends Component {
 
     DEFAULT_STATE = {
-        data: []
+        data: [],
+        properties: [],
+        departments: [],
+        property: 0,
+        department: 0,
+        employee: '',
+        startDate: '',
+        endDate: ''
     }
 
     constructor(props) {
@@ -21,6 +27,8 @@ class PunchesReport extends Component {
     }
     componentWillMount() {
         this.getReport();
+        this.getDepartments();
+        this.getProperties();
     }
 
     getReport = () => {
@@ -28,7 +36,8 @@ class PunchesReport extends Component {
             this.props.client
                 .query({
                     query: GET_REPORT_QUERY,
-                    fetchPolicy: 'no-cache'
+                    fetchPolicy: 'no-cache',
+                    variables: { ...this.getFilters() }
                 })
                 .then(({ data }) => {
                     this.setState(() => ({
@@ -40,8 +49,78 @@ class PunchesReport extends Component {
                     this.setState(() => ({ loadingReport: false }));
                 });
         })
-
     }
+
+    getDepartments = () => {
+        this.setState(() => ({ loadingDepartments: true }), () => {
+            this.props.client
+                .query({
+                    query: GET_DEPARTMENTS_QUERY,
+                    variables: { Id_Entity: this.state.property },
+                    fetchPolicy: 'no-cache'
+                })
+                .then(({ data }) => {
+                    this.setState(() => ({
+                        departments: data.catalogitem,
+                        loadingDepartments: false
+                    }));
+                })
+                .catch(error => {
+                    this.setState(() => ({ loadingDepartments: false }));
+                });
+        })
+    }
+
+    getProperties = () => {
+        this.setState(() => ({ loadingProperties: true }), () => {
+            this.props.client
+                .query({
+                    query: GET_PROPERTIES_QUERY,
+                    fetchPolicy: 'no-cache'
+                })
+                .then(({ data }) => {
+                    this.setState(() => ({
+                        properties: data.getbusinesscompanies,
+                        loadingProperties: false
+                    }));
+                })
+                .catch(error => {
+                    this.setState(() => ({ loadingProperties: false }));
+                });
+        })
+    }
+
+    getFilters = () => {
+        var filters = {}, { property, department, employee, startDate, endDate } = this.state;
+
+        if (property)
+            filters = { ...filters, idEntity: property };
+        if (department)
+            filters = { ...filters, Id_Department: department };
+        if (employee)
+            filters = { ...filters, employee };
+        if (startDate)
+            filters = { ...filters, startDate };
+        if (endDate)
+            filters = { ...filters, endDate };
+
+        return filters;
+    }
+
+    updateFilter = ({ property, department, employee, startDate, endDate }) => {
+        this.setState((prevState) => ({
+            property,
+            department: prevState.property != property ? 0 : department,
+            employee,
+            startDate,
+            endDate,
+            departments: prevState.property != property ? [] : prevState.departments
+        }), () => {
+            this.getDepartments();
+            this.getReport();
+        });
+    }
+
     render() {
         const { loadingReport } = this.state;
         const loading = loadingReport;
@@ -51,7 +130,7 @@ class PunchesReport extends Component {
             <div className="row">
                 <div className="col-md-12">
                     <div className="card">
-                        <Filter />
+                        <Filter {...this.state} updateFilter={this.updateFilter} />
                         <Table data={this.state.data} />
                     </div>
                 </div>
--- src/components/PunchesReport/queries.js
+++ src/components/PunchesReport/queries.js
@@ -1,9 +1,9 @@
 import gql from 'graphql-tag';
 
 export const GET_REPORT_QUERY = gql`
-query punches{
-    punches{
-     employeeId
+  query punches($idEntity: Int,$Id_Department: Int,$employee: String,$startDate: Date,$endDate: Date ){
+    punches(idEntity:$idEntity,Id_Department: $Id_Department,employee: $employee,startDate: $startDate,endDate: $endDate){
+      employeeId
       name
       hourCategory
       hoursWorked
@@ -15,7 +15,28 @@ query punches{
       lunchOut
       hotelCode
       positionCode
-      
+    }
+  }
+`;
+
+export const GET_PROPERTIES_QUERY = gql`
+  query hotels {
+    getbusinesscompanies( IsActive: 1, Contract_Status: "'C'", Id_Parent : -1) {
+      Id
+      Id_Parent
+      Name
+      Country
+      State
+      City
+    }
+  }
+`;
+
+export const GET_DEPARTMENTS_QUERY = gql`
+  query department($Id_Entity: Int){
+    catalogitem(Id_Catalog:8, Id_Entity:$Id_Entity){
+      Id
+      DisplayLabel
     }
   }
 `;
