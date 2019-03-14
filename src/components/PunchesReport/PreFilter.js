692b1e9462c65d5e98aa0531d8a47ed2a49d8f09
Gershell López <grlopez90@gmail.com>
2019-03-14 16:20:28 -0600

    prefilter using react-select

--- src/components/PunchesReport/PreFilter.js
+++ src/components/PunchesReport/PreFilter.js
@@ -4,6 +4,15 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { GET_USER, GET_PROPERTY_BY_REGION } from './queries';
 import withApollo from 'react-apollo/withApollo';
+import Select from 'react-select';
+import makeAnimated from 'react-select/lib/animated';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+    paper: { overflowY: 'unset' },
+    container: { overflowY: 'unset' }
+};
 
 class PreFilter extends Component {
 
@@ -12,7 +21,8 @@ class PreFilter extends Component {
         this.state = {
             properties: [],
             openModal: true,
-            property: 0
+            property: 0,
+            options: []
         }
     }
 
@@ -33,6 +43,7 @@ class PreFilter extends Component {
     getPropertiesByRegion = (IdRegion) => {
 
         if (IdRegion == 0) return false;
+        let options = [];
 
         this.props.client.query({
             query: GET_PROPERTY_BY_REGION,
@@ -40,6 +51,11 @@ class PreFilter extends Component {
         }).then(({ data }) => {
             this.setState({
                 properties: data.getbusinesscompanies
+            }, () => {
+                this.state.properties.map((property) => {
+                    options.push({ value: property.Id, label: property.Id + " | " + property.Name })
+                });
+                this.setState({ options: options });
             });
         });
     }
@@ -66,33 +82,55 @@ class PreFilter extends Component {
         });
     }
 
+    handleChangePreFilter = (properties) => {
+        this.setState({
+            properties
+        }, () => {
+            this.setState({
+                property: properties.value
+            })
+        });
+    };
+
     render() {
+        const { classes } = this.props;
         return (
-            <Dialog maxWidth="md" open={this.state.openModal} onClose={this.props.handleCloseModal}>
+            <Dialog maxWidth="md" open={this.state.openModal} onClose={this.props.handleCloseModal} classes={{ paper: classes.paper }}>
                 <DialogTitle style={{ padding: '0px' }}>
                     <div className="modal-header">
                         <h5 className="modal-title">Prefilter Punches</h5>
                     </div>
                 </DialogTitle>
-                <DialogContent>
-                    <div className="card-body">
+                <DialogContent style={{ overflowY: "unset" }}>
+                    <div className="card-body" style={{ minWidth: "300px" }}>
                         <form action="" onSubmit={this.changeFilter}>
-                            <select type="text" className="form-control" name="property" required onChange={this.handleChange}>
+                            {/* <select type="text" className="form-control" name="property" required onChange={this.handleChange}>
                                 <option value="">Select a Property</option>
                                 {this.state.properties.map((property) => {
                                     return <option value={property.Id}>{property.Name}</option>
                                 })}
-                            </select>
+                            </select> */}
+                            <Select
+                                options={this.state.options}
+                                value={""}
+                                onChange={this.handleChangePreFilter}
+                                closeMenuOnSelect={false}
+                                components={makeAnimated()}
+                            />
                             <button type="submit" className="btn btn-success mt-2 float-right">
                                 Filter
                             </button>
                         </form>
                     </div>
                 </DialogContent>
-            </Dialog>
+            </Dialog >
         );
     }
 
 }
 
-export default withApollo(PreFilter);
+PreFilter.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(withApollo(PreFilter));
