c766449e96e08f964bc43e783fc7759aa833d2f5
Jorge Corea <jcorear94@outlook.com>
2019-05-07 17:34:00 -0600

    Saving property generates user, completed

--- src/components/Company/TabsInDialog/GeneralInforProperty.js
+++ src/components/Company/TabsInDialog/GeneralInforProperty.js
@@ -13,6 +13,8 @@ import './valid.css';
 import AutosuggestInput from 'ui-components/AutosuggestInput/AutosuggestInput';
 import ConfirmDialog from 'material-ui/ConfirmDialog';
 import LocationForm from '../../ui-components/LocationForm';
+import { INSERT_USER_QUERY } from '../User/Mutations';
+import moment from 'moment';
 
 class GeneralInfoProperty extends Component {
     constructor(props) {
@@ -315,8 +317,7 @@ class GeneralInfoProperty extends Component {
                         }
                     })
                     .then((data) => {
-                        NewIdRegion = data.data.inscatalogitem.Id;
-
+                        NewIdRegion = data.data.inscatalogitem.Id;                         
                     })
                     .catch((error) => {
                         this.props.handleOpenSnackbar('error', 'Error: Inserting Department: ' + error);
@@ -391,6 +392,47 @@ class GeneralInfoProperty extends Component {
                         })
                         .then(({ data }) => {
                             this.props.updateIdProperty(parseInt(data.insbusinesscompanies.Id), parseInt(data.insbusinesscompanies.Id_Parent));
+      
+                            this.props.client
+                                .mutate({
+                                    mutation: INSERT_USER_QUERY,
+                                    variables: {
+                                        input: {                                            
+                                                Id_Entity: 1,
+                                                Id_Contact: null,
+                                                Id_Roles: 1,
+                                                Full_Name: "''",
+                                                Electronic_Address: `'${this.state.email}'`,
+                                                AllowDelete:  1,
+                                                AllowEdit:  1,
+                                                AllowExport:  1,
+                                                AllowInsert:  1	,
+                                                Id_Language: 194,
+                                                Code_User: `'${this.state.Code}'`,
+                                                Date_Created:  "'2018-08-14'",
+                                                Date_Updated:  "'2018-08-14'",
+                                                IdRegion:  null,	
+                                                IsActive:  1,
+                                                IsAdmin:  1,
+                                                Phone_Number: `'${this.state.phoneNumber}'`,
+                                                User_Created:  1,
+                                                User_Updated:  1,
+                                                IsRecruiter: 0,
+                                                IdRegion: null,
+                                                IdSchedulesManager: null,
+                                                IdSchedulesEmployees: null,
+                                                isEmployee: 0                                            
+                                            }
+                                        }
+                                    })
+                                    .then((data) => {
+                                        alert('saved')
+                                        console.log(data);
+                                    })
+                                    .catch((error) => {
+                                        alert('Error');
+                                        console.log(error);
+                                    });                        
 
                             this.setState({
                                 linearProgress: false
@@ -954,7 +996,7 @@ class GeneralInfoProperty extends Component {
                         id={`${property}_edit`}
                         type="button"
                         onClick={() => {
-                            this.setState({ [enableEdit]: false, [`${property}`]: this.state[`${property}Original`] });
+                            this.setState({ [enableEdit]: false, [`${property}`]: this.state[`${property}Original`] });                                            
                         }}
                     >
                         <i className="fas fa-ban" />
