58493002b6932b061fc28c95346c7b4a5a171d2e
Luis <luisfajardo0789@gmail.com>
2019-07-09 17:09:44 -0600

    Add Timer.js

---
+++ src/components/Visit/Timer.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import moment from 'moment';
+import {getDefaultTime, getTime, durationToTime} from './Utilities';
+
+let interval = null;
+
+class Timer extends Component {
+    
+    constructor(props) {
+        super(props)
+        this.state={
+            time: getDefaultTime()
+        }
+    }
+
+    handleTime() {
+        this.setState((prevState) => {
+            return { time: moment(prevState.time).add(1, 'seconds') }
+        })
+    }
+
+    handleRun = (props) => {
+        let { run } = props;
+
+        if(interval !== null) clearInterval(interval);
+
+        if(run) {
+            interval=setInterval(()=>this.handleTime(),1000);
+        } 
+    }
+    
+    
+    componentWillReceiveProps(nextProp){
+        let { duration, startTime, endTime } = nextProp;
+        this.setState(() => {
+            return { time: durationToTime(startTime, duration.days, duration.hours, duration.minutes, duration.seconds) }
+        }, () => this.handleRun(nextProp));
+    }
+
+    render() {
+        let days = this.props.duration.days;
+        let daysMsg = !!days ? days + ' days - ' : '';
+        return (
+            <h5>
+                {daysMsg + moment(this.state.time).format("HH:mm:ss")}
+            </h5>
+        )
+    }
+}
+
+export default Timer;
