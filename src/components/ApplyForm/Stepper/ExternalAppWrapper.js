c0c598bac7d9a3fd7c711b27412fbc9e7f3869b5
Jorge Corea <jcorear94@outlook.com>
2019-07-17 09:28:30 -0600

    Ext App Wrapper

---
+++ src/components/ApplyForm/Stepper/ExternalAppWrapper.js
@@ -0,0 +1,8 @@
+import React, {Fragment} from 'react';
+import StepperApplyForm from './StepperApplyForm';
+
+const ExternalAppWrapper = props => {
+    return <Fragment></Fragment>
+}
+
+export default ExternalAppWrapper;
