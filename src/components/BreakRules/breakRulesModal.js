1fb23d3f276bf66e391657b608b507456542b4c4
Jorge Corea <jcorear94@outlook.com>
2019-09-11 17:05:16 -0600

    Break Rules fixes completed. Able to select "All Employees" at once

--- src/components/BreakRules/breakRulesModal.js
+++ src/components/BreakRules/breakRulesModal.js
@@ -30,6 +30,8 @@ class BreakRulesModal extends Component {
         
         selectedEmployees: [],
         employeeSelectOptions: [],
+        allSelectableEmployees: [],
+        shouldSelectAllEmployees: false,
         
         placeBreakOptions: [
             { value: 'middle', label: 'Middle of Shift' },
@@ -66,9 +68,14 @@ class BreakRulesModal extends Component {
     }
 
     componentWillReceiveProps(nextProps) {
-        let employeeSelectOptions = nextProps.employeeList.map(employee => {
-            return { value: employee.id, key: employee.id, label: `${employee.firstName} ${employee.lastName}` }
-        });
+        let employeeSelectOptions = [
+            { value: 0, key: 0, label: `All Employees`},
+            ...nextProps.employeeList.map(employee => {
+                return { value: employee.id, key: employee.id, label: `${employee.firstName} ${employee.lastName}` }
+            })
+        ]
+
+        const allSelectableEmployees = nextProps.employeeList.map(employee => employee.id);
 
         if(nextProps.isRuleEdit && nextProps.breakRuleToEdit) {
             const { id, name, code, lenght, employee_BreakRule, isAutomatic, breakRuleDetail, isActive, isPaid } = nextProps.breakRuleToEdit;
@@ -104,7 +111,8 @@ class BreakRulesModal extends Component {
         }
 
         this.setState({
-            employeeSelectOptions
+            employeeSelectOptions,
+            allSelectableEmployees
         })
     }
 
@@ -184,11 +192,27 @@ class BreakRulesModal extends Component {
     }
 
     handleChangeEmployeesSelect = (selectedEmployees) => {
-        this.setState({ selectedEmployees });
+        const selectAllOption = selectedEmployees.find(item => item.value === 0);
+
+        if(selectAllOption && !this.state.shouldSelectAllEmployees){
+            this.setState({ selectedEmployees: [selectAllOption], shouldSelectAllEmployees: true });   
+            return;         
+        }
+        
+        if(selectedEmployees.length > 1){
+            this.setState({ selectedEmployees: [...selectedEmployees.filter(item => item.value > 0)], shouldSelectAllEmployees: false });            
+            return;
+        } 
     }    
 
     handleSubmit = e => {
         e.preventDefault();
+
+        const employeeList = this.state.shouldSelectAllEmployees ? 
+                             this.state.allSelectableEmployees : 
+                             this.state.selectedEmployees.map(employee => {
+                                 return employee.value
+                             });
 
         if(this.state.ruleId === 0){
             this.props.client.mutate({
@@ -204,9 +228,7 @@ class BreakRulesModal extends Component {
                         isActive: true 
                     },
     
-                    employees: this.state.selectedEmployees.map(employee => {
-                        return employee.value
-                    })
+                    employees: employeeList
                 }
             }).then(({data}) => {
                 //If its an automatic break, save the config as a break rule detail
@@ -254,9 +276,7 @@ class BreakRulesModal extends Component {
                         isActive: this.state.isActive 
                     },
     
-                    employees: this.state.selectedEmployees.map(employee => {
-                        return employee.value
-                    })
+                    employees: employeeList
                 }
             }).then(({data}) => {
                 if(this.state.isAutomaticBreak) {
