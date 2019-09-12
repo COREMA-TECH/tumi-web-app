501d339694ff870f40ef645b6d12c7db328d94fd
Lucho <luisfajardo0789@gmail.com>
2019-09-11 18:19:30 -0600

    Advancing style view

--- src/components/Security/Roles/index.js
+++ src/components/Security/Roles/index.js
@@ -1,3 +1,15 @@
-import RolesForm from './RolesForm';
+import React, { Component } from 'react';
 
-export default RolesForm;
+class Roles extends Component {
+    render() {
+        return (
+            <div>sdfsd</div>
+        );
+    }
+}
+
+export default Roles;
+
+// TODO: (LF) Quitar codigo comentado
+//import RolesForm from './RolesForm';
+//export default RolesForm;
